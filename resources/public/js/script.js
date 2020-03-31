ec89d629e3a4b9d18c540de6bae67b03d2cdbf65
Gary Johnson <lambdatronic@gmail.com>
2020-03-31 15:18:21 -0400

    Adding Dan Keenan's static home page files.

---
+++ resources/public/js/script.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    var headerHeight = $('.wrapper-navbar').outerHeight();
+	var homeheader = $( window ).height() - headerHeight;
+  $('.header-image').css('height', homeheader);
+	
+	
+});
