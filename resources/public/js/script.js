14f159d6585394276c5d0de5813773473b8324fe
Daniel Keenan <hatchbackcreative@gmail.com>
2020-04-03 15:58:20 -0700

    Stylistic and filename changes per Gary and 404 page add.

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,14 +1,13 @@
-
-$.fn.equalHeights = function(){
+$.fn.equalHeights = function(){ 
 	var max_height = 0;
-	$(this).each(function(){
+	$(this).each(function(){ 
 		max_height = Math.max($(this).height(), max_height);
 	});
-	$(this).each(function(){
+	$(this).each(function(){ 
 		$(this).height(max_height);
 	});
 };
-$(document).ready(function() {
+$(document).ready(function() { 
     var headerHeight = $('.wrapper-navbar').outerHeight();
     var homeheader = $(window).height() - headerHeight;
     $('.header-image').css('height', homeheader);
@@ -22,7 +21,4 @@ $(document).ready(function() {
 		$(".bottom-menu").toggleClass("bottom-animate");
     });
 	    $('.box-shadow').equalHeights();
-
-	
-   
 });
