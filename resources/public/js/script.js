9ee368dc0c7fd959ec253be728cb46caec2e94e0
Matt Spencer <sirmspencer@gmail.com>
2020-04-07 14:48:44 -0700

    clean up script.js

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,13 +1,13 @@
-$.fn.equalHeights = function(){ 
+$.fn.equalHeights = function () {
     var max_height = 0;
-    $(this).each(function(){
+    $(this).each(function () {
         max_height = Math.max($(this).height(), max_height);
     });
-    $(this).each(function(){
+    $(this).each(function () {
         $(this).height(max_height);
     });
 };
-$(document).ready(function() { 
+$(document).ready(function () {
     var headerHeight = $('.wrapper-navbar').outerHeight();
     var homeheader = $(window).height() - headerHeight;
     $('.header-image').css('height', homeheader);
