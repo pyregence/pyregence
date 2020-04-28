8c0bd439e60ad2c0c07a2af03cf7c41d77dc3c61
Daniel Keenan <hatchbackcreative@gmail.com>
2020-04-28 13:00:52 -0700

    updates per Matt

--- resources/public/js/team-filter.js
+++ resources/public/js/team-filter.js
@@ -1,32 +1,22 @@
-filterSelection("all")
 function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("team-block");
     if (c == "all") c = "";
     for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) {
+            w3AddClass(x[i], "show");
+        } else {
+            w3RemoveClass(x[i], "show");
+        }
     }
 }
 function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-    }
+    element.className = element.className.replace(name, "") + " " + name
 }
 function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
+    element.className = element.className.replace(name, "")
 }
+filterSelection("all")
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
