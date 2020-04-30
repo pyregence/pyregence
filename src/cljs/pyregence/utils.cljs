0f0b4703b3ff58788306608a90ee7b845c008929
Gary Johnson <lambdatronic@gmail.com>
2020-04-29 21:24:25 -0400

    Rewrote pyregence.utils/call-remote! methods using fetch and fetch-and-process.

--- src/cljs/pyregence/utils.cljs
+++ src/cljs/pyregence/utils.cljs
@@ -1,7 +1,7 @@
 (ns pyregence.utils
   (:require [cljs.reader :as edn]
             [clojure.string :as str]
-            [clojure.core.async :refer [chan go >! <! close!]]
+            [clojure.core.async :refer [go <!]]
             [cljs.core.async.interop :refer-macros [<p!]]))
 
 (defn input-value
@@ -93,51 +93,45 @@
 
 (defmulti call-remote! (fn [method url data] method))
 
-;; FIXME: Replace with calls to fetch as defined above.
 (defmethod call-remote! :get [_ url data]
-  (let [query-string (->> data
-                          (map (fn [[k v]] (str (pr-str k) "=" (pr-str v))))
-                          (str/join "&")
-                          (js/encodeURIComponent))
-        fetch-params {:method "get"
-                      :headers {"Accept" "application/edn"
-                                "Content-Type" "application/edn"}}
-        result-chan  (chan)]
-    (-> (.fetch js/window
-                (str url
-                     "?auth-token=883kljlsl36dnll9s9l2ls8xksl"
-                     (when (= query-string "") (str "&" query-string)))
-                (clj->js fetch-params))
-        (.then  (fn [response]   (if (.-ok response) (.text response) (.reject js/Promise response))))
-        (.then  (fn [edn-string] (go (>! result-chan (or (edn/read-string edn-string) :no-data)))))
-        (.catch (fn [response]   (js/console.log response) (close! result-chan))))
-    result-chan))
+  (go
+    (let [query-string (->> data
+                            (map (fn [[k v]] (str (pr-str k) "=" (pr-str v))))
+                            (str/join "&")
+                            (js/encodeURIComponent))
+          fetch-params {:method "get"
+                        :headers {"Accept" "application/edn"
+                                  "Content-Type" "application/edn"}}
+          edn-string   (<! (fetch-and-process (str url
+                                                   "?auth-token=883kljlsl36dnll9s9l2ls8xksl"
+                                                   (when (not= query-string "") (str "&" query-string)))
+                                              fetch-params
+                                              (fn [response] (.text response))))]
+      (or (edn/read-string edn-string) :no-data))))
 
-;; FIXME: Replace with calls to fetch as defined above.
 ;; Combines status and error message into return value
 (defmethod call-remote! :post [_ url data]
-  (let [fetch-params {:method "post"
-                      :headers (merge {"Accept" "application/edn"}
-                                      (when-not (= (type data) js/FormData)
-                                        {"Content-Type" "application/edn"}))
-                      :body (cond
-                              (= js/FormData (type data)) data
-                              data                        (pr-str data)
-                              :else                       nil)}
-        result-chan  (chan)]
-    (-> (.fetch js/window
-                (str url "?auth-token=883kljlsl36dnll9s9l2ls8xksl")
-                (clj->js fetch-params))
-        (.then  (fn [response] (.then (.text response)
-                                      (fn [text]
-                                        (go (>! result-chan {:success (.-ok response)
-                                                             :status  (.-status response)
-                                                             :message (or (edn/read-string text) "")}))))))
-        (.catch (fn [edn-string] (go (>! result-chan {:success false :message (or (edn/read-string edn-string) "")})))))
-    result-chan))
+  (go
+    (let [fetch-params {:method "post"
+                        :headers (merge {"Accept" "application/edn"}
+                                        (when-not (= (type data) js/FormData)
+                                          {"Content-Type" "application/edn"}))
+                        :body (cond
+                                (= js/FormData (type data)) data
+                                data                        (pr-str data)
+                                :else                       nil)}
+          response      (<! (fetch (str url "?auth-token=883kljlsl36dnll9s9l2ls8xksl")
+                                   fetch-params))]
+      (if response
+        {:success (.-ok response)
+         :status  (.-status response)
+         :message (or (edn/read-string (<p! (.text response))) "")}
+        {:success false
+         :status  nil
+         :message ""}))))
 
 (defmethod call-remote! :default [method _ _]
-  (throw (str "No such method (" method ") defined for pyregence.utils/call-remote!")))
+  (throw (ex-info (str "No such method (" method ") defined for pyregence.utils/call-remote!") {})))
 
 (defn call-clj-async! [clj-fn-name & args]
   (call-remote! :post
