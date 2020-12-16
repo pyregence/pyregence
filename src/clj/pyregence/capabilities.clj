bd9f135a8792843ed1081165593d5af613aaece4
Matt Spencer <sirmspencer@gmail.com>
2020-12-15 18:15:59 -0800

    Add removal service

--- src/clj/pyregence/capabilities.clj
+++ src/clj/pyregence/capabilities.clj
@@ -4,7 +4,7 @@
             [clojure.set        :as set]
             [clj-http.client    :as client]
             [pyregence.database :refer [call-sql]]
-            [pyregence.logging  :refer [log-str]]
+            [pyregence.logging  :refer [log log-str]]
             [pyregence.views    :refer [data-response]]))
 
 ;;; State
@@ -113,9 +113,10 @@
 
 (defn set-capabilities! []
   (try
-    (process-layers!)
-    (log-str (count @layers) " layers added to capabilities.")
-    (catch Exception e
+    (let [stdout? (= 0 (count @layers))]
+      (process-layers!)
+      (log (str (count @layers) " layers added to capabilities.") :force-stdout? stdout?))
+    (catch Exception _
       (log-str "Failed to load capabilities."))))
 
 (defn remove-workspace! [workspace-name]
--- src/clj/pyregence/logging.clj
+++ src/clj/pyregence/logging.clj
@@ -7,12 +7,14 @@
 
 (defonce synchronized-log-writer (agent nil))
 
-(defonce output-path (atom ""))
+(defonce output-path      (atom ""))
+(defonce clean-up-service (atom nil))
 
 (defn max-length [string length]
   (subs string 0 (min length (count string))))
 
-(defn log [data & {:keys [newline? pprint?] :or {newline? true pprint? false}}]
+(defn log [data & {:keys [newline? pprint? force-stdout?]
+                   :or {newline? true pprint? false force-stdout? false}}]
   (let [timestamp    (.format (SimpleDateFormat. "MM/dd HH:mm:ss") (Date.))
         log-filename (str (.format (SimpleDateFormat. "YYYY-MM-dd") (Date.)) ".log")
         max-data     (max-length data 500)
@@ -21,19 +23,43 @@
                           (if pprint? (pp/pprint max-data) max-data)
                           (when newline? "\n"))]
     (send-off synchronized-log-writer
-              (if (pos? (count @output-path))
-                (fn [_] (spit (str @output-path log-filename) line :append true))
-                (fn [_] (print line)))))
+              (if (or force-stdout?
+                      (.exists (io/file @output-path)))
+                (fn [_] (print line))
+                (fn [_] (spit (str @output-path log-filename) line :append true)))))
   nil)
 
 (defn log-str [& data]
   (log (apply str data)))
 
+(defn- start-clean-up-service! []
+  (log "Starting log file removal service." :force-stdout? true)
+  (future
+    (while true
+      (Thread/sleep (* 1000 60 60 24)) ; 24 hours in milliseconds.
+      (try (doseq [file (as-> (io/file @output-path) files
+                          (.listFiles files)
+                          (sort-by #(.lastModified %) files)
+                          (take (- (count files) 10) files))]
+             (io/delete-file file))
+           (catch Exception _)))))
+
 (defn set-output-path! [path]
-  (let [path (str path (when-not (str/ends-with? path "/") "/"))]
+  (let [path (str path
+                  (when (and (pos? (count path))
+                             (not  (str/ends-with? path "/")))
+                    "/"))]
     (try
       (io/make-parents (str path "dummy.log"))
-      (log-str "Logging to: " path)
       (reset! output-path path)
+      (log (str "Logging to: " path) :force-stdout? true)
       (catch Exception _
-        (log-str "Error setting log path to " path ". Check that you supplied a valid path.")))))
+        (log (str "Error setting log path to " path ". Check that you supplied a valid path.") :force-stdout? true)))
+    (cond
+      (and (nil? @clean-up-service) (.exists (io/file @output-path)))
+      (start-clean-up-service!)
+
+      (not (or (nil? @clean-up-service) (.exists (io/file @output-path))))
+      (do
+        (future-cancel @clean-up-service)
+        (reset! clean-up-service nil)))))
