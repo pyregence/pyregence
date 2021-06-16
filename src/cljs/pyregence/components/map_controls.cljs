1d72ef6cc69efed7abde64b2313cf71284d4aa26
RJ Sheperd <rjsheperd@gmail.com>
2021-06-16 14:22:03 -0700

    Fix queue errors - only search jobs within last hour

--- src/cljs/pyregence/components/map_controls.cljs
+++ src/cljs/pyregence/components/map_controls.cljs
@@ -367,13 +367,13 @@
                  (reset! show-camera? false))
             @show-info?])
          ;; Remove access to Match-Drop for Prod
-         ;; (when (and (number? user-id) (not mobile?))
-         ;;   [:flame
-         ;;    (str (hs-str @show-match-drop?) " match drop tool")
-         ;;    #(do (swap! show-match-drop? not)
-         ;;         (set-show-info! false)
-         ;;         (reset! show-camera? false))
-         ;;    @show-match-drop?])
+         (when (and (number? user-id) (not mobile?))
+           [:flame
+            (str (hs-str @show-match-drop?) " match drop tool")
+            #(do (swap! show-match-drop? not)
+                 (set-show-info! false)
+                 (reset! show-camera? false))
+            @show-match-drop?])
          (when-not mobile?
            [:camera
             (str (hs-str @show-camera?) " cameras")
