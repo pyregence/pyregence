50d42b5e9e59c9640ff9066c7b40842d4d1ad541
Matt Spencer <sirmspencer@gmail.com>
2021-07-09 10:08:54 -0700

    changes from Gary

--- src/clj/pyregence/compile_cljs.clj
+++ src/clj/pyregence/compile_cljs.clj
@@ -4,31 +4,28 @@
             [cljs.main       :as compiler]))
 
 (defn- delete-folder
-  "Empty contents of all folders, then delete folders"
+  "Delete a folder and all of its contents recursively."
   [folder-name]
   (let [folder (io/file folder-name)]
     (when (.exists folder)
       (doseq [file (reverse (file-seq folder))]
         (io/delete-file file)))))
 
-(defn- pre-compile [opts-file]
-  (if-let [dir (as-> (slurp opts-file) target
-                 (edn/read-string target)
-                 (get target :output-dir))]
-    (do (delete-folder dir)
-        :success)
-    (println opts-file "is missing :output-dir")))
+(defn- pre-compile [opts-file-name]
+  (if-let [folder-name (as-> (slurp opts-file-name) opts
+                         (edn/read-string opts)
+                         (get opts :output-dir))]
+    (delete-folder folder-name)
+    (str opts-file-name " is missing :output-dir.")))
 
 (defn -main
   "A custom wrapper for compilation that allows pre-compile actions."
-  [& [opts-file]]
-  (if opts-file ;; TODO, when moving to triangulum, use triangulum.cli/get-cli-options
-    (let [opts-file-io (io/file (-> opts-file))]
-      (cond
-        (not (.exists opts-file-io))
-        (println "The supplied compiler options file does not exist.")
-
-        (pre-compile opts-file)
-        (compiler/-main "-co" opts-file "-c")))
-    (println "A compiler options file is required to compile.\n\n   Usage: clojure -M:compile-cljs compile-prod.cljs.edn"))
+  [& [opts-file-name]]
+  (if-not opts-file-name ; TODO, when moving to triangulum, use triangulum.cli/get-cli-options
+    (println "A compiler options file is required to compile.\n\n   Usage: clojure -M:compile-cljs compile-prod.cljs.edn")
+    (if-not (.exists (io/file opts-file-name))
+      (println "The supplied compiler options file does not exist.")
+      (if-let [error-msg (pre-compile opts-file-name)]
+        (println error-msg)
+        (compiler/-main "-co" opts-file-name "-c"))))
   (shutdown-agents))
