fbafc5c3d83e004050c7daa695205842a07a725f
RJ Sheperd <rjsheperd@gmail.com>
2021-08-10 13:32:12 -0700

    Fix compilation error

--- src/clj/pyregence/views.clj
+++ src/clj/pyregence/views.clj
@@ -77,7 +77,7 @@
                  [:div#app]]
                 [:script {:type "text/javascript"}
                  (str "window.onload = function () { pyregence.client.init("
-                      (json/write-str (assoc (:params request)
+                      (json/write-str (assoc params
                                              :mapbox    (get-config :mapbox)
                                              :features  (get-config :features)
                                              :geoserver (get-config :geoserver)))
