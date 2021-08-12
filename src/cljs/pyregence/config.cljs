e97a103cbad317f4fada13c310c321166e0bf9ae
RJ Sheperd <rjsheperd@gmail.com>
2021-08-12 11:50:45 -0700

    PYR-532 Add comments re: projections

--- src/cljs/pyregence/config.cljs
+++ src/cljs/pyregence/config.cljs
@@ -326,7 +326,11 @@
        "&SRSNAME=EPSG:3857"
        "&BBOX=" (str/join "," extent) ",EPSG:3857"))
 
-(defn wms-layer-url [layer]
+(defn wms-layer-url
+  "Generates a Web Mapping Service (WMS) url to download a PNG tile.
+
+   Mapbox GL requires tiles to be projected to EPSG:3857 (Web Mercator)."
+  [layer]
   (str (wms-url)
        "?SERVICE=WMS"
        "&VERSION=1.3.0"
@@ -341,7 +345,12 @@
        "&BBOX={bbox-epsg-3857}"
        "&LAYERS=" layer))
 
-(defn wfs-layer-url [layer]
+(defn wfs-layer-url
+  "Generates a Web Feature Service (WFS) url to download an entire vector data
+   set as GeoJSON.
+
+   Mapbox GL does support GeoJSON in EPSG:4326. However, it does not support WFS."
+  [layer]
   (str (wfs-url)
        "?SERVICE=WFS"
        "&VERSION=1.3.0"
@@ -350,7 +359,14 @@
        "&SRSNAME=EPSG:4326"
        "&TYPENAME=" layer))
 
-(defn mvt-layer-url [layer]
+(defn mvt-layer-url
+  "Generates a Mapbox Vector Tile (MVT) URL to be used with with Mapbox GL.
+
+   When adding MVT data, the projection must be EPSG:3857 (Web Mercator) or
+   EPSG:900913 (Google Web Mercator). EPSG:900913 is used since GeoServer's
+   embedded [GeoWebCache](https://www.geowebcache.org/) Web Map Tile Service (WMTS)
+   supports EPSG:900913 by default, but does not support EPSG:3857 by default."
+  [layer]
   (str (mvt-url)
        "?REQUEST=GetTile"
        "&SERVICE=WMTS"
