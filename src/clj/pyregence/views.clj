e8069e62c924dcddf73aa80156d7fb2d506e7df3
Oliver-BE <obaldwinedwards@gmail.com>
2021-08-13 15:36:20 -0700

    Replacing old Bootstrap file with new, smaller bare-bones one.

--- src/clj/pyregence/views.clj
+++ src/clj/pyregence/views.clj
@@ -25,10 +25,8 @@
    [:meta {:charset "utf-8"}]
    [:meta {:name    "viewport"
            :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
-   [:link {:rel         "stylesheet"
-           :href        "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-           :integrity   "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-           :crossorigin "anonymous"}]
+   [:link {:href "css/bootstrap.css"
+           :rel  "stylesheet"}]
    [:link {:rel "stylesheet" :href "css/style.css"}]
    [:link {:rel "icon" :type "image/png" :href "/images/favicon.png"}]
    [:script {:async true :src "https://www.googletagmanager.com/gtag/js?id UA-168639214-1"}]
