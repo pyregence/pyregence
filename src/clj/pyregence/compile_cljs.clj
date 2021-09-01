9d442be6450660ae063d1823eae4226367a1d3bc
Matt Spencer <sirmspencer@gmail.com>
2021-09-01 11:06:43 -0700

    fix compile hooks

--- src/clj/pyregence/compile_cljs.clj
+++ src/clj/pyregence/compile_cljs.clj
@@ -11,12 +11,18 @@
       (doseq [file (reverse (file-seq folder))]
         (io/delete-file file)))))
 
-(defn- pre-compile [opts-file-name]
+(defn- pre-compile-hook [opts-file-name]
   (if-let [folder-name (as-> (slurp opts-file-name) opts
                          (edn/read-string opts)
                          (get opts :output-dir))]
     (delete-folder folder-name)
     (str opts-file-name " is missing :output-dir.")))
+
+(defn post-figwheel-hook [config]
+  (if-let [output-to (get-in config [:options :output-to])]
+    (spit (io/file (get-in config [:options :output-dir]) "manifest.edn")
+          {output-to output-to})
+    (println "Error reading figwheel config.")))
 
 (defn -main
   "A custom wrapper for compilation that allows pre-compile actions."
@@ -25,7 +31,7 @@
     (println "A compiler options file is required to compile.\n\n   Usage: clojure -M:compile-cljs compile-prod.cljs.edn")
     (if-not (.exists (io/file opts-file-name))
       (println "The supplied compiler options file does not exist.")
-      (if-let [error-msg (pre-compile opts-file-name)]
+      (if-let [error-msg (pre-compile-hook opts-file-name)]
         (println error-msg)
         (compiler/-main "-co" opts-file-name "-c"))))
   (shutdown-agents))
