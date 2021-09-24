c4336aad358ddc350d73baab5d09976bfbdf0ada
Oliver-BE <obaldwinedwards@gmail.com>
2021-09-24 15:41:27 -0400

    PYR-606: Reduce margin-top for data link.

--- src/cljs/pyregence/components/map_controls.cljs
+++ src/cljs/pyregence/components/map_controls.cljs
@@ -298,8 +298,7 @@
 
 (defn- help-section []
   [:section#help-section {:style {:width "100%"}}
-   [:article {:style {:margin-bottom "0.5rem"
-                      :margin-top    "4rem"
+   [:article {:style {:margin        "0.5rem 0"
                       :padding-left  "1rem"
                       :padding-right "1rem"}}
     [:div {:style {:background      ($/color-picker :transparent)
