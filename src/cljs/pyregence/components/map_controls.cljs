9729bf4d8e1127e6ac16f7571ed71055c624700e
Oliver-BE <obaldwinedwards@gmail.com>
2021-10-06 19:43:26 -0700

    Checking red flag API instead of Mapbox for red flag warnings.

--- src/cljs/pyregence/components/map_controls.cljs
+++ src/cljs/pyregence/components/map_controls.cljs
@@ -408,23 +408,20 @@
 ;; Red Flag Warning
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn- add-red-flag-layer! []
-  (go
-    (let [data (-> (<! (u/call-clj-async! "get-red-flag-layer"))
-                   (:body)
-                   (js/JSON.parse))]
-      (mb/create-red-flag-layer! "red-flag" data))))
-
 (defn toggle-red-flag-layer!
   "Toggle the red-flag warning layer"
   [show-red-flag?]
   (swap! show-red-flag? not)
-  (if (not (mb/layer-exists? "red-flag"))
-    (do
-      (toast-message! "There are no red flag warnings at this time.")
-      (reset! show-red-flag? false))
-    (when @show-red-flag?
-      (add-red-flag-layer!)))
+  (go
+    (let [data (-> (<! (u/call-clj-async! "get-red-flag-layer"))
+                   (:body)
+                   (js/JSON.parse))]
+      (if (not (some? data))
+        (do
+          (toast-message! "There are no red flag warnings at this time.")
+          (reset! show-red-flag? false))
+        (when (and @show-red-flag? (not (mb/layer-exists? "red-flag")))
+          (mb/create-red-flag-layer! "red-flag" data)))))
   (mb/set-visible-by-title! "red-flag" @show-red-flag?)
   (mb/clear-popup! "red-flag"))
 
