c4e9c387a8bb364dc1cf0b3e8b2f136e9a68efc5
Oliver-BE <obaldwinedwards@gmail.com>
2021-10-25 13:56:34 -0700

    Get rid of body component in static pages.

--- src/cljs/pyregence/pages/help.cljs
+++ src/cljs/pyregence/pages/help.cljs
@@ -4,14 +4,11 @@
 ;; UI Components
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn- body []
-  [:div {:id "help-page"}
-   [:h1 {:style {:margin     "5rem 2.5rem"
-                 :text-align "center"}}
-    "This page is under construction. Please check back later!"]])
-
 (defn root-component
   "The root component of the /help page."
   [_]
   (fn [_]
-    [body]))
+    [:div {:id "help-page"}
+     [:h1 {:style {:margin     "5rem 2.5rem"
+                   :text-align "center"}}
+      "This page is under construction. Please check back later!"]]))
--- src/cljs/pyregence/pages/not_found.cljs
+++ src/cljs/pyregence/pages/not_found.cljs
@@ -1,16 +1,13 @@
 (ns pyregence.pages.not-found)
 
-(defn- body []
-  [:div {:style {:margin-top "100px"}}
-   [:div {:style {:align-content   "center"
-                  :display         "flex"
-                  :justify-content "center"
-                  :margin-bottom   "6rem"}}
-    [:h1 {:style {:text-align "center"}}
-     "404 - Page Not Found"]]])
-
 (defn root-component
   "The root component for the 404 page."
   [_]
   (fn [_]
-    [body]))
+    [:div {:style {:margin-top "100px"}}
+     [:div {:style {:align-content   "center"
+                    :display         "flex"
+                    :justify-content "center"
+                    :margin-bottom   "6rem"}}
+      [:h1 {:style {:text-align "center"}}
+       "404 - Page Not Found"]]]))
