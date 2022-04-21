305a47df67a8523a678155c0cb5c1ebbe8bd8542
Oliver-BE <obaldwinedwards@gmail.com>
2022-04-21 15:53:00 -0700

    PYR-764 Apply new map control changes manually to each individual component file.

--- src/cljs/pyregence/components/map_controls/information_tool.cljs
+++ src/cljs/pyregence/components/map_controls/information_tool.cljs
@@ -1,6 +1,7 @@
 (ns pyregence.components.map-controls.information-tool
   (:require  [reagent.core     :as r]
              [reagent.dom      :as rd]
+             [clojure.set      :as set]
              [pyregence.state  :as !]
              [pyregence.utils  :as u]
              [pyregence.config :as c]
@@ -20,7 +21,7 @@
                                :z-index  "1"}}
    [:label message]])
 
-(defn- information-div [units info-height]
+(defn- information-div [units info-height convert]
   (r/create-class
    {:component-did-mount
     (fn [this]
@@ -32,11 +33,17 @@
 
     :render
     (fn [_]
-      [:div {:style {:bottom "0" :position "absolute" :width "100%"}}
-       [:label {:style {:margin-top ".5rem" :text-align "center" :width "100%"}}
-        (str (:band (get @!/last-clicked-info @!/*layer-idx)) (u/clean-units units))]])}))
+      (let [cleaned-last-clicked-info (u/replace-no-data-nil @!/last-clicked-info
+                                                             @!/no-data-quantities)
+            current-point             (get cleaned-last-clicked-info @!/*layer-idx)]
+        [:div {:style {:bottom "0" :position "absolute" :width "100%"}}
+         [:label {:style {:margin-top ".5rem" :text-align "center" :width "100%"}}
+          (if-let [value (:band current-point)]
+            (str (if (fn? convert) (convert value) value)
+                 (u/clean-units units))
+            "No info available for this timestep.")]]))}))
 
-(defn- vega-information [box-height box-width select-layer! units cur-hour]
+(defn- vega-information [box-height box-width select-layer! units cur-hour convert]
   (r/with-let [info-height (r/atom 0)]
     [:<>
      [vega-box
@@ -44,10 +51,21 @@
       box-width
       select-layer!
       units
-      cur-hour]
-     [information-div units info-height]]))
+      cur-hour
+      convert]
+     [information-div units info-height convert]]))
 
-(defn- single-point-info [box-height _ units convert]
+(defn- fbfm40-info []
+  [:div {:style {:margin "0.125rem 0.75rem"}}
+   [:p {:style {:margin-bottom "0.125rem"
+                :text-align    "center"}}
+    [:strong "Fuel Type: "]
+    (get-in c/fbfm40-lookup [@!/last-clicked-info :fuel-type])]
+   [:p {:style {:margin-bottom "0"}}
+    [:strong "Description: "]
+    (get-in c/fbfm40-lookup [@!/last-clicked-info :description])]])
+
+(defn- single-point-info [box-height _ units convert no-convert]
   (let [legend-map  (u/mapm (fn [li] [(js/parseFloat (get li "quantity")) li]) @!/legend-list)
         legend-keys (sort (keys legend-map))
         color       (or (get-in legend-map [(-> @!/last-clicked-info
@@ -58,7 +76,22 @@
                           (when (and high low)
                             (u/interp-color (get-in legend-map [low "color"])
                                             (get-in legend-map [high "color"])
-                                            (/ (- @!/last-clicked-info low) (- high low))))))]
+                                            (/ (- @!/last-clicked-info low) (- high low))))))
+        *inputs     (->> @!/*params
+                         (@!/*forecast)
+                         (vals)
+                         (into #{}))
+        add-units   #(u/end-with % (u/clean-units units))
+        fbfm40?     (contains? *inputs :fbfm40)
+        display-val (cond
+                      fbfm40? ; for all fbfm40 layers we just need a simple lookup
+                      (get-in legend-map [@!/last-clicked-info "label"])
+
+                      (and (fn? convert) (empty? (set/intersection no-convert *inputs))) ; convert the value
+                      (add-units (convert @!/last-clicked-info))
+
+                      :else ; otherwise, do not convert
+                      (add-units @!/last-clicked-info))]
     [:div {:style {:align-items     "center"
                    :display         "flex"
                    :flex-direction  "column"
@@ -73,30 +106,30 @@
                      :height           "1.5rem"
                      :margin-right     "0.5rem"
                      :width            "1.5rem"}}]
-      [:h4 (u/end-with (or (get-in legend-map [@!/last-clicked-info "label"])
-                           (if (fn? convert) (convert @!/last-clicked-info) @!/last-clicked-info))
-                       (u/clean-units units))]]
-     (when (some #(= "TU1" (get % "label")) @!/legend-list)
-       [:div {:style {:margin "0.125rem 0.75rem"}}
-        [:p {:style {:margin-bottom "0.125rem"
-                     :text-align    "center"}}
-         [:strong "Fuel Type: "]
-         (get-in c/fbfm40-lookup [@!/last-clicked-info :fuel-type])]
-        [:p {:style {:margin-bottom "0"}}
-         [:strong "Description: "]
-         (get-in c/fbfm40-lookup [@!/last-clicked-info :description])]])]))
+      [:h4 display-val]]
+     (when fbfm40?
+       [fbfm40-info])]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Root component
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn information-tool [get-point-info!
-                        parent-box
-                        select-layer!
-                        units
-                        convert
-                        cur-hour
-                        close-fn!]
+(defn information-tool
+  "The point information tool component. Supports both single point info and
+   multiple point info. The units, convert, and no-convert arguments
+   are set in the :options key of a forecasts options map in config.cljs.
+   For example, the :ch options key on the near term forecast fuels tab specifes
+   that the units are meters, the value returned from get-point-info! should be
+   converted using the provided function, and the value from get-point-info!
+   should not be converted when using the :cfo Source."
+  [get-point-info!
+   parent-box
+   select-layer!
+   units
+   convert
+   no-convert
+   cur-hour
+   close-fn!]
   (r/with-let [click-event (mb/add-single-click-popup! #(get-point-info! (mb/get-overlay-bbox)))]
     [:div#info-tool
      [resizable-window
@@ -106,11 +139,15 @@
       "Point Information"
       close-fn!
       (fn [box-height box-width]
-        (let [has-point?   (mb/get-overlay-center)
-              loading-info [loading-cover
-                            box-height
-                            box-width
-                            "Loading..."]]
+        (let [has-point?    (mb/get-overlay-center)
+              single-point? (number? @!/last-clicked-info)
+              no-info?      (if single-point?
+                              (contains? @!/no-data-quantities (str @!/last-clicked-info))
+                              (or (empty? @!/last-clicked-info)
+                                  (->> @!/last-clicked-info
+                                    (map (fn [entry]
+                                           (contains? @!/no-data-quantities (str (:band entry)))))
+                                    (every? true?))))]
           (cond
             (not has-point?)
             [loading-cover
@@ -119,7 +156,10 @@
              "Click on the map to view the value(s) of particular point."]
 
             @!/point-info-loading?
-            loading-info
+            [loading-cover
+             box-height
+             box-width
+             "Loading..."]
 
             (and (nil? @!/last-clicked-info) (empty? @!/legend-list))
             [loading-cover
@@ -133,29 +173,27 @@
              box-width
              "There was an issue getting the legend for this layer."]
 
-            (and (number? @!/last-clicked-info)
-                 (>= @!/last-clicked-info -50))
-            [single-point-info
-             box-height
-             box-width
-             units
-             convert]
-
-            (or (< @!/last-clicked-info -50)
-                (-> @!/last-clicked-info (first) (:band) (< -50)))
+            no-info?
             [loading-cover
              box-height
              box-width
              "This point does not have any information."]
 
-            (and (not-empty @!/last-clicked-info) (not-empty @!/legend-list))
+            single-point?
+            [single-point-info
+             box-height
+             box-width
+             units
+             convert
+             no-convert]
+
+            :else
             [vega-information
              box-height
              box-width
              select-layer!
              units
-             cur-hour]
-
-            :else loading-info)))]]
+             cur-hour
+             convert])))]]
     (finally
       (mb/remove-event! click-event))))
