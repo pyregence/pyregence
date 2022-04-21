be60a07705dbec8290b42ad1b7a92a3120a4256b
Oliver-BE <obaldwinedwards@gmail.com>
2022-04-21 10:55:57 -0700

    PYR-761 Update the order of the inputs on the Fuels tab.

--- src/cljs/pyregence/config.cljs
+++ src/cljs/pyregence/config.cljs
@@ -65,50 +65,7 @@
                   :underlays     (merge common-underlays near-term-forecast-underlays)
                   :time-slider?  false
                   :hover-text    "Layers related to fuel and potential fire behavior."
-                  :params        {:model {:opt-label  "Source"
-                                          :hover-text [:p {:style {:margin-bottom "0"}}
-                                                       "Stock "
-                                                       [:strong "LANDFIRE 2.0.0"]
-                                                       " data ("
-                                                       [:a {:href   "https://landfire.gov"
-                                                            :target "_blank"}
-                                                        "https://landfire.gov"]
-                                                       ") at 30 m resolution."
-                                                       [:br]
-                                                       [:br]
-                                                       [:strong "California Forest Observatory"]
-                                                       " – Summer 2020 at 10 m resolution. Courtesy of the California Forest Observatory ("
-                                                       [:a {:href   "https://forestobservatory.com"
-                                                            :target "_blank"}
-                                                        "https://forestobservatory.com"]
-                                                       "), © Salo Sciences, Inc. 2020."
-                                                       [:br]
-                                                       [:br]
-                                                       [:strong "2021 California fuelscape"]
-                                                       " prepared by Pyrologix, LLC ("
-                                                       [:a {:href   "https://pyrologix.com"
-                                                            :target "_blank"}
-                                                        "https://pyrologix.com"]
-                                                       "), 2021."
-                                                       [:br]
-                                                       [:br]
-                                                       [:strong "California Ecosystem Climate Solutions"]
-                                                       " - Data provided by the "
-                                                       [:a {:href   "https://california-ecosystem-climate.solutions/"
-                                                            :target "_blank"}
-                                                        "California Ecosystem Climate Solutions"]
-                                                       ", Wang et al. (2021)."]
-
-                                          :options    {:landfire      {:opt-label "LANDFIRE 2.0.0"
-                                                                       :filter    "landfire-2.0.0"}
-                                                       :cfo           {:opt-label "California Forest Obs."
-                                                                       :filter    "cfo-2020"}
-                                                       :ca-fuelscapes {:opt-label "2021 CA fuelscape"
-                                                                       :filter    "ca-fuelscapes"}
-                                                       :cec           {:opt-label    "CA Ecosystem Climate Solutions"
-                                                                       :filter       "cec"
-                                                                       :disabled-for #{:asp :slp :dem :cc :ch :cbh :cbd}}}}
-                                  :layer {:opt-label  "Layer"
+                  :params        {:layer {:opt-label  "Layer"
                                           :hover-text [:p {:style {:margin-bottom "0"}}
                                                        "Geospatial surface and canopy fuel inputs, forecasted ember ignition probability and head fire spread rate & flame length."
                                                        [:br]
@@ -164,6 +121,48 @@
                                                                 :no-convert      #{:cfo}
                                                                 :reverse-legend? true
                                                                 :disabled-for    #{:cec}})}
+                                  :model {:opt-label  "Source"
+                                                          :hover-text [:p {:style {:margin-bottom "0"}}
+                                                                       "Stock "
+                                                                       [:strong "LANDFIRE 2.0.0"]
+                                                                       " data ("
+                                                                       [:a {:href   "https://landfire.gov"
+                                                                            :target "_blank"}
+                                                                        "https://landfire.gov"]
+                                                                       ") at 30 m resolution."
+                                                                       [:br]
+                                                                       [:br]
+                                                                       [:strong "California Forest Observatory"]
+                                                                       " – Summer 2020 at 10 m resolution. Courtesy of the California Forest Observatory ("
+                                                                       [:a {:href   "https://forestobservatory.com"
+                                                                            :target "_blank"}
+                                                                        "https://forestobservatory.com"]
+                                                                       "), © Salo Sciences, Inc. 2020."
+                                                                       [:br]
+                                                                       [:br]
+                                                                       [:strong "2021 California fuelscape"]
+                                                                       " prepared by Pyrologix, LLC ("
+                                                                       [:a {:href   "https://pyrologix.com"
+                                                                            :target "_blank"}
+                                                                        "https://pyrologix.com"]
+                                                                       "), 2021."
+                                                                       [:br]
+                                                                       [:br]
+                                                                       [:strong "California Ecosystem Climate Solutions"]
+                                                                       " - Data provided by the "
+                                                                       [:a {:href   "https://california-ecosystem-climate.solutions/"
+                                                                            :target "_blank"}
+                                                                        "California Ecosystem Climate Solutions"]
+                                                                       ", Wang et al. (2021)."]
+                                                          :options    {:landfire      {:opt-label "LANDFIRE 2.0.0"
+                                                                                       :filter    "landfire-2.0.0"}
+                                                                       :cfo           {:opt-label "California Forest Obs."
+                                                                                       :filter    "cfo-2020"}
+                                                                       :ca-fuelscapes {:opt-label "2021 CA fuelscape"
+                                                                                       :filter    "ca-fuelscapes"}
+                                                                       :cec           {:opt-label    "CA Ecosystem Climate Solutions"
+                                                                                       :filter       "cec"
+                                                                                       :disabled-for #{:asp :slp :dem :cc :ch :cbh :cbd}}}}
                                   :model-init {:opt-label  "Model Creation Time"
                                                :hover-text "Time the data was created."
                                                :options    {:loading {:opt-label "Loading..."}}}}}
