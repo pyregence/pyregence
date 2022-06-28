8f704f721126ff204c33d94e328569625ab872a2
Joel Iniguez <joel.iniguez@gmail.com>
2022-06-27 17:26:53 -0700

    Add feedback changes

--- src/cljs/pyregence/state.cljs
+++ src/cljs/pyregence/state.cljs
@@ -27,12 +27,11 @@ the `:underlays` map (which defines the optional layers available on the given t
 (which is used to define each input box and the options available in that input box on the side-panel for the given tab).
 The processed output from `process-capabilities!`:
 notably contains any fire names and/or user layers (obtained from the back-end) that are added to the capabilities atom
-(since the fire names and user layers are not defined initially in config.cljs and thus the options atom)."}
+(since the fire names and user layers are not defined initially in config.cljs."}
   capabilities (r/atom {}))
-(defonce ^{:doc " Contains the map associated with the :params key inside of the currently selected forecast in the capabilities atom.
-For example, the processed-params for a user on the Weather tab would be a map containing the :band, :model, and :model-init maps."}
+(defonce ^{:doc "A vector of maps containing all of the layers for the current forecast sorted by hour. These layers are obtained from the back-end through the capabilities.clj/get-layers function which returns all layers that match a given set of strings for filtering. For example, if the user is on the Weather tab looking at the Temperature (F) forecast, param-layers will be a vector of length 145 where each entry in the vector (which is a map) contains information about one Temperature (F) layer. A length of 145 also indicates that there are 145 different time steps in this specific forecast."}
   param-layers (r/atom []))
-(defonce ^{:doc "Contains the populated form field values to populate the `Layer Selection` form on the side-panel."}
+(defonce ^{:doc "Contains the map associated with the :params key inside of the currently selected forecast in the capabilities atom. For example, the processed-params for a user on the Weather tab would be a map containing the :band, :model, and :model-init maps."}
   processed-params (r/atom []))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
