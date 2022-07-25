630d487c66c6049e295b27a4d71d54ad4ca43b07
Joel <joel.iniguez@gmail.com>
2022-07-25 10:26:40 -0700

    PYR1-679: Add "Navigation Bar" and "Forecast Tabs" components (#729)
    * Sort require entries and add nav-bar dependency
    
    * Add nav-bar, forecast-tabs, login-menu components
    
    * Replace forecast-tab logic with component instance
    
    * Decouple components from state and delegate to NTF
    
    The near-term-forecast builds the props necessary for rendering
    forecast-tabs, login-menu, and nav-bar
    
    * PR feedback: minor adjustments
    
    * Fix dangling closing bracket
    
    * Adjust Login position, reduce `top` position value
    
    * Remove padding and unused nav-container
    
    * PR Feedback: remove unused funcs, tweaks & gotchas
    
    * Rename admin-list -> user-org-list
    
    * Update user-org-list docstring description

---
+++ src/cljs/pyregence/components/forecast_tabs.cljs
@@ -0,0 +1,27 @@
+(ns pyregence.components.forecast-tabs
+  (:require [pyregence.components.common :refer [tool-tip-wrapper]]))
+
+(defn- $forecast-label [selected?]
+  (merge
+   {:cursor "pointer"
+    :margin "0 1rem 0 1rem"}
+   (when selected? {:color "white"})))
+
+(defn forecast-tabs
+  "Declares a component that displayes interactive tabs for selecting distinct forecasts"
+  [{:keys [capabilities current-forecast select-forecast! user-org-list]}]
+  [:div {:style {:display "flex" :padding ".25rem 0"}}
+   (doall
+    (map (fn [[key {:keys [allowed-org hover-text opt-label]}]]
+           (when (or (nil? allowed-org)
+                     (some (fn [{org-name :opt-label}]
+                             (= org-name allowed-org))
+                           user-org-list))
+             ^{:key key}
+             [tool-tip-wrapper
+              hover-text
+              :top
+              [:label {:style    ($forecast-label (= current-forecast key))
+                       :on-click #(select-forecast! key)}
+               opt-label]]))
+         capabilities))])
---
+++ src/cljs/pyregence/components/login_menu.cljs
@@ -0,0 +1,32 @@
+(ns pyregence.components.login-menu
+  (:require [clojure.core.async                 :refer [<! go]]
+            [pyregence.components.common        :refer [tool-tip-wrapper]]
+            [pyregence.components.svg-icons     :as svg]
+            [pyregence.styles                   :as $]
+            [pyregence.utils                    :as u]))
+
+(defn login-menu
+  "A login and logout navigation menu item"
+  [{:keys [is-admin? logged-in? mobile?]}]
+  [:div {:style {:position "absolute" :right "3rem"}}
+   (when-not mobile?
+     (if logged-in?
+       [:div {:style {:align-items "center" :display "flex"}}
+        (when is-admin?
+          [tool-tip-wrapper
+           "Visit the admin page"
+           :top
+           [:a {:style      ($/combine ($/fixed-size "1.5rem")
+                                       {:cursor "pointer" :margin-right "1rem"})
+                :aria-label "Visit the admin page"
+                :href       "/admin"}
+            [svg/admin-user]]])
+        [:label {:style    {:cursor "pointer" :margin ".16rem .2rem 0 0"}
+                 :on-click (fn []
+                             (go (<! (u/call-clj-async! "log-out"))
+                                 (-> js/window .-location .reload)))}
+         "Log Out"]]
+       ;; [:label {:style {:margin-right "1rem" :cursor "pointer"}
+       ;;          :on-click #(u-browser/jump-to-url! "/register")} "Register"]
+       [:label {:style    {:cursor "pointer" :margin ".16rem .2rem 0 0"}
+                :on-click #(u/jump-to-url! "/login")} "Log In"]))])
