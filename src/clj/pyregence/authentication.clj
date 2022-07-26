ad3c5c23355582c8cb9d894720b5e60703a57b64
Joel Iniguez <joel.iniguez@gmail.com>
2022-07-26 12:36:34 -0700

    Update add-new-user: loosen email restriction
    `add-new-user` now takes an `opts` where an :email-restriction? can be specified

--- src/clj/pyregence/authentication.clj
+++ src/clj/pyregence/authentication.clj
@@ -28,17 +28,21 @@
     (data-response "" {:session {:user-id (:user_id user)}})
     (data-response "" {:status 403})))
 
-(defn add-new-user [email name password]
-  (let [default-settings (pr-str {:timezone :utc})
-        new-user-id      (nil-on-error
-                          (sql-primitive (call-sql "add_new_user"
-                                                   {:log? false}
-                                                   email
-                                                   name
-                                                   password
-                                                   default-settings)))]
+(defn add-new-user [email name password opts]
+  (let [{:keys [org-id restrict-email?]
+         :or   {org-id nil restrict-email? true}} opts
+        default-settings               (pr-str {:timezone :utc})
+        new-user-id                    (nil-on-error
+                                        (sql-primitive (call-sql "add_new_user"
+                                                                 {:log? false}
+                                                                 email
+                                                                 name
+                                                                 password
+                                                                 default-settings)))]
     (if new-user-id
-      (do (call-sql "auto_add_org_user" new-user-id (re-find #"@{1}.+" email))
+      (do (if (and org-id (not restrict-email?))
+            (call-sql "add_org_user" org-id new-user-id)
+            (call-sql "auto_add_org_user" new-user-id (re-find #"@{1}.+" email)))
           (data-response ""))
       (data-response "" {:status 403}))))
 
