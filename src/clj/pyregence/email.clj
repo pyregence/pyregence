4b0dfc1d2164e5d711a17b26c7cfe1ebfb40ee76
Oliver-BE <obaldwinedwards@gmail.com>
2022-12-13 17:34:05 -0800

    PYR1-769 Send an email upon Match Drop completion

--- src/clj/pyregence/email.clj
+++ src/clj/pyregence/email.clj
@@ -3,22 +3,33 @@
   (:require [triangulum.config   :refer [get-config]]
             [triangulum.database :refer [call-sql]]
             [pyregence.views     :refer [data-response]]
+            [pyregence.utils     :refer [convert-date-string]]
             [postal.core         :refer [send-message]]))
 
 ;; TODO get name for greeting line.
-(defn get-password-reset-message [url email reset-key]
+(defn- get-password-reset-message [base-url email reset-key]
   (str "Hi " email ",\n\n"
        "  To reset your password, simply click the following link:\n\n"
-       "  " url "/reset-password?email=" email "&reset-key=" reset-key "\n\n"
+       "  " base-url "/reset-password?email=" email "&reset-key=" reset-key "\n\n"
        "  - Pyregence Technical Support"))
 
-(defn get-new-user-message [url email reset-key]
+(defn- get-new-user-message [base-url email reset-key]
   (str "Hi " email ",\n\n"
        "  You have been registered for Pyregence. Please verify your email by clicking the following link:\n\n"
-       "  " url "/verify-email?email=" email "&reset-key=" reset-key "\n\n"
+       "  " base-url "/verify-email?email=" email "&reset-key=" reset-key "\n\n"
        "  - Pyregence Technical Support"))
 
-(defn send-reset-key-email! [mail-config email subject message-fn]
+(defn- get-match-drop-message [base-url email {:keys [match-job-id display-name fire-name ignition-time]}]
+  (str "Hi " email ",\n\n"
+       "  Your Match Drop with ID \"" match-job-id "\" and display-name \"" display-name
+       "\" has finished running. Please click the following link to view it "
+       "on Pyrcast:\n\n" base-url "/forecast?zoom=5.5&forecast=active-fire&fuel="
+       "landfire&output=burned&model-init=" (convert-date-string ignition-time)
+       "&layer-idx=0&lat=37.22&fire-name=" fire-name "&lng=-118.79&pattern=all&"
+       "model=elmfire \n\n"
+       "  - Pyregence Technical Support"))
+
+(defn- send-reset-key-email! [mail-config email subject message-fn]
   (let [reset-key (str (UUID/randomUUID))
         result    (send-message
                    (dissoc mail-config :site-url)
@@ -29,15 +40,32 @@
     (call-sql "set_reset_key" email reset-key)
     (data-response email {:status (when-not (= :SUCCESS (:error result)) 400)})))
 
-(defn send-email [email email-type]
+(defn- send-match-drop-email! [mail-config email subject message-fn match-drop-args]
+  (let [result (send-message
+                (dissoc mail-config :site-url)
+                {:from    (mail-config :user)
+                 :to      email
+                 :subject subject
+                 :body    (message-fn (:site-url mail-config) email match-drop-args)})]
+    (if (= :SUCCESS (:error result))
+      (data-response "Match Drop email successfully sent.")
+      (data-response "There was an issue sending the Match Drop email." {:status 400}))))
+
+(defn send-email! [email email-type & [match-drop-args]]
   (let [mail-config (get-config :mail)]
     (condp = email-type
-      :reset    (send-reset-key-email! mail-config
-                                       email
-                                       "Pyregence Password Reset"
-                                       get-password-reset-message)
-      :new-user (send-reset-key-email! mail-config
-                                       email
-                                       "Pyregence New User"
-                                       get-new-user-message)
-      (data-response "Invalid email type." {:status 400}))))
+      :reset      (send-reset-key-email! mail-config
+                                         email
+                                         "Pyregence Password Reset"
+                                         get-password-reset-message)
+      :new-user   (send-reset-key-email! mail-config
+                                         email
+                                         "Pyregence New User"
+                                         get-new-user-message)
+      :match-drop (send-match-drop-email! mail-config
+                                          email
+                                          "Match Drop Finished Running"
+                                          get-match-drop-message
+                                          match-drop-args)
+      (data-response "Invalid email type. Options are `:reset`, `:new-user`, or `:match-drop.`"
+                     {:status 400}))))
--- src/clj/pyregence/utils.clj
+++ src/clj/pyregence/utils.clj
@@ -1,4 +1,8 @@
-(ns pyregence.utils)
+(ns pyregence.utils
+  (:import  [java.util TimeZone]
+            [java.text SimpleDateFormat])
+  (:require [triangulum.database :refer [call-sql sql-primitive]]
+            [pyregence.views     :refer [data-response]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Utility Functions
@@ -9,3 +13,19 @@
   [& body]
   (let [_ (gensym)]
     `(try ~@body (catch Exception ~_ nil))))
+
+(defn convert-date-string
+  "Converts a date str in the format of '2022-12-01 18:00 UTC' to '20221201_180000'."
+  [date-str]
+  (let [in-format  (SimpleDateFormat. "yyyy-MM-dd HH:mm z")
+        out-format (doto (SimpleDateFormat. "yyyyMMdd_HHmmss")
+                     (.setTimeZone (TimeZone/getTimeZone "UTC")))]
+    (->> date-str
+         (.parse in-format)
+         (.format out-format))))
+
+(defn get-email-by-user-id [user-id]
+  (if-let [email (sql-primitive (call-sql "get_email_by_user_id" user-id))]
+    (data-response email)
+    (data-response (str "There is no user with the id " user-id)
+                   {:status 403})))
