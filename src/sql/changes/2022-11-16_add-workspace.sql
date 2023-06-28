35d6aabbbe8cf319a8e087dfca613f9eafd29769
Oliver-BE <obaldwinedwards@gmail.com>
2023-06-28 14:57:11 -0700

    Remove already applied SQL change files

--- src/sql/changes/2022-11-16_add-workspace.sql
+++
@@ -1,3 +0,0 @@
--- Add the geoserver_workspace column to match_jobs
-ALTER TABLE match_jobs
-ADD COLUMN geoserver_workspace text;
--- src/sql/changes/2022-11-30_match-drop-access.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE users
-ADD match_drop_access boolean DEFAULT FALSE;
-
-UPDATE users
-SET match_drop_access = TRUE
-WHERE email = 'admin@all-orgs.com';
