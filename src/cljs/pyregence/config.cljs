1c27a49121bf08e2dd71d0d0ad149c9626c2a820
Oliver-BE <obaldwinedwards@gmail.com>
2024-04-24 16:08:55 -0400

    Add NorthWestern Energy

--- src/cljs/pyregence/config.cljs
+++ src/cljs/pyregence/config.cljs
@@ -401,7 +401,7 @@
                                                               :crown-fire-area {:opt-label    "Crown fire area"
                                                                                 :filter       "crown-fire-area"
                                                                                 :units        "Acres"
-                                                                                :disabled-for #{:tlines :nve :liberty}}
+                                                                                :disabled-for #{:tlines :nve :liberty :northwestern}}
                                                               :plignrate       {:opt-label    "Power line ignition rate"
                                                                                 :filter       "plignrate"
                                                                                 :units        "Ignitions/line-mi/hr"
