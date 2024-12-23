61c6b6be0204018de3973c79d1220583891401a3
drew verlee <drew.verlee@gmail.com>
2024-12-22 19:03:21 -0600

    add selecting default Risk Ignition Pattern through option directly
    This change adds the ability to select a default Risk Ignition Pattern
    (e.g UtilityCompany Energy Lines) by updating the database's
    
    Organization Layers table layer_config column to have contain a string
    that's a valid edn with a top level key :selected? set to true. e.g
    
    layer_config                                                                           |
    ---------------------------------------------------------------------------------------|
    {:selected? true :opt-label "UtilityCompany", :filter "...", :geoserver-key "..."}|
    
    This works because that selected key will be available in the panel_drowdown
    because it's assoced into state along the Organization Layer tabels layer_path. e.g
    
    layer_path                                          |
    ----------------------------------------------------|
    [:fire-risk :params :pattern :options :UtilityCompany]|
    
    And this is available on a per company selection
    
    |org_layer_uid|organization_rid|
    |-------------|----------------|
    |98           |20              |
    
    The major downside here is it introduces yet another way to select
    the default, it takes precedence over the :default-option found in the config.cljs
    which is used as the "val" in the panel_dropdown.
    
    Why not use the :default-option? Because that will require a change to how we
    collect the state of the layer_config. Will explore that option in another commit.

--- src/cljs/pyregence/components/map_controls/panel_dropdown.cljs
+++ src/cljs/pyregence/components/map_controls/panel_dropdown.cljs
@@ -4,6 +4,20 @@
             [pyregence.styles               :as $]
             [pyregence.utils.dom-utils      :as u-dom]))
 
+(defn- options->selected-key
+  "given a map of selected-keys to hiccup option tags , return the selected-key
+  associated with the option tag that has the selected? key. This solves the
+  problem of selecting an option through the option (as opposed to the parent),
+  but then passing that information to the parent :select so it can use it in
+  the :select/value which react uses instead of an html option selected
+  attribute"
+  [options]
+  (reduce-kv
+   (fn [_ potential-key {:keys [selected?]}]
+     (when selected? (reduced potential-key)))
+   nil
+   options))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Root component
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -20,7 +34,7 @@
                                :fill   ($/color-picker :font-color)})}
       [svg/help]]]]
    [:select {:style     ($/dropdown)
-             :value     (or val :none)
+             :value     (or (options->selected-key options) val :none)
              :disabled  disabled?
              :on-change #(call-back (u-dom/input-keyword %))}
     (map (fn [[key {:keys [opt-label enabled? disabled-for]}]]
