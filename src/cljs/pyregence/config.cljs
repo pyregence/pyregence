3cf00bdf17f2f5893ac17b801e79b024f9122179
drew verlee <drew.verlee@gmail.com>
2025-01-09 12:09:39 -0600

    try to align map columns...

--- src/cljs/pyregence/config.cljs
+++ src/cljs/pyregence/config.cljs
@@ -206,8 +206,8 @@
                                   :model-init {:opt-label        "Model Creation Time"
                                                :hover-text       "Time the data was created."
                                                :options          {:loading {:opt-label "Loading..."}}}
-                                  :timezone   {:default            :local
-                                               :change identity}}}
+                                  :timezone   {:default :local
+                                               :change  identity}}}
    :fire-weather {:opt-label       "Weather"
                   :filter          "fire-weather-forecast"
                   :geoserver-key   :shasta
@@ -563,16 +563,16 @@
                                                                          :opt-label "GridFire"
                                                                          :filter    "gridfire"}}}
                                     :model-init {:opt-label  "Forecast Start Time"
-                                                 :hover-text       "This shows the date and time (24 hour time) from which the prediction starts. To view a different start time, select one from the dropdown menu. This data is automatically updated when active fires are sensed by satellites."
-                                                 :disabled         (fn [selected-set]
-                                                                     (some (->> selected-set
-                                                                                (vals)
-                                                                                (filter keyword?)
-                                                                                (set))
-                                                                           #{:active-fires}))
-                                                 :options          {:loading {:opt-label "Loading..."}}}
+                                                 :hover-text "This shows the date and time (24 hour time) from which the prediction starts. To view a different start time, select one from the dropdown menu. This data is automatically updated when active fires are sensed by satellites."
+                                                 :disabled   (fn [selected-set]
+                                                               (some (->> selected-set
+                                                                          (vals)
+                                                                          (filter keyword?)
+                                                                          (set))
+                                                                     #{:active-fires}))
+                                                 :options    {:loading {:opt-label "Loading..."}}}
                                     :timezone   {:default :local
-                                                 :change identity}}}
+                                                 :change  identity}}}
    :psps-zonal   {:opt-label       "PSPS"
                   :filter          "psps-zonal"
                   :geoserver-key   :psps
