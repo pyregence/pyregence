21c9b522675ae263edfee705936d1bb92806d0d3
Oliver-BE <obaldwinedwards@gmail.com>
2025-01-28 13:51:50 -0500

    PYR1-1019 Small PNM update

--- src/cljs/pyregence/config.cljs
+++ src/cljs/pyregence/config.cljs
@@ -406,7 +406,7 @@
                                                               :crown-fire-area {:opt-label    "Crown fire area"
                                                                                 :filter       "crown-fire-area"
                                                                                 :units        "Acres"
-                                                                                :disabled-for #{:tlines :nve :liberty :northwestern :otec}}
+                                                                                :disabled-for #{:tlines :nve :liberty :northwestern :otec :pnm}}
                                                               :plignrate       {:opt-label    "Power line ignition rate"
                                                                                 :filter       "plignrate"
                                                                                 :units        "Ignitions/line-mi/hr"
