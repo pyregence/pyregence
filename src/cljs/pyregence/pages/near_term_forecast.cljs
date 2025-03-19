a2d56364adbd9937a425b55b32217a0ac7879b95
Daniel Silva <dsilva@sig-gis.com>
2025-03-19 15:53:02 -0300

    maybe this is the right usage

--- src/cljs/pyregence/pages/near_term_forecast.cljs
+++ src/cljs/pyregence/pages/near_term_forecast.cljs
@@ -530,7 +530,7 @@
   (go
     (!/set-state-legend-list! [])
     (reset! !/last-clicked-info nil)
-    (js/gtag "select-forecast" (str key))
+    (js/gtag "event" "select-forecast" (str key))
     (reset! !/*forecast key)
     (reset! !/processed-params (get-forecast-opt :params))
     (mb/set-multiple-layers-visibility! #"isochrones" false) ; hide isochrones underlay when switching tabs
