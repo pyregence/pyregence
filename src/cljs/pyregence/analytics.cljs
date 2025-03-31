17c08e3284eef7109278f7389e364ad6e4e403de
Daniel Silva <dsilva@sig-gis.com>
2025-03-31 14:57:06 -0300

    fixup the gtag "api"

--- src/cljs/pyregence/analytics.cljs
+++ src/cljs/pyregence/analytics.cljs
@@ -2,15 +2,15 @@
 
 (defn gtag
   "Call `gtag` with a given `info` (a clojure map with the info we want to track)"
-  [info]
-  (js/gtag "event" "tool-usage" (clj->js info)))
+  [event-name info]
+  (js/gtag "event" event-name (clj->js info)))
 
 (defn gtag-tool-clicked
   "Call `gtag` for a specific togglable tool named `tool-name`
   `gtag` is a function provided by google analytics tool.
   See https://developers.google.com/analytics/devguides/collection/ga4"
   ([event-name]
-   (gtag {:tool-clicked event-name}))
+   (gtag "tool-usage" {:tool-clicked event-name}))
   ([show? tool-name]
    (let [event-name (str (if show? "show-" "hide-") tool-name)]
      (gtag-tool-clicked event-name))))
