2725a51205b3f1069be0ba1d1d4b9141a268159d
Daniel Silva <dsilva@sig-gis.com>
2025-03-31 14:33:55 -0300

    compose

--- src/cljs/pyregence/analytics.cljs
+++ src/cljs/pyregence/analytics.cljs
@@ -1,14 +1,14 @@
 (ns pyregence.analytics)
 
+(defn gtag
+  "Call `gtag` with a given `info` (a clojure map with the info we want to track)"
+  [info]
+  (js/gtag "event" "registered-user" (clj->js info)))
+
 (defn gtag-tool
   "Call `gtag` for a specific togglable tool named `tool-name`
   `gtag` is a function provided by google analytics tool.
   See https://developers.google.com/analytics/devguides/collection/ga4"
   [show? tool-name]
   (let [event-name (str (if show? "show-" "hide-") tool-name)]
-    (js/gtag "event" "registered-user" (clj->js {:tool-clicked event-name}))))
-
-(defn gtag
-  "Call `gtag` with a given `info` (a clojure map with the info we want to track)"
-  [info]
-  (js/gtag "event" "registered-user" (clj->js info)))
+    (gtag {:tool-clicked event-name})))
