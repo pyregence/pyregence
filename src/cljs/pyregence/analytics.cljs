eac06fe90147c1102232ab8fba94f2a826e0b88b
Daniel Silva <dsilva@sig-gis.com>
2025-03-31 14:55:38 -0300

    fixup event name

--- src/cljs/pyregence/analytics.cljs
+++ src/cljs/pyregence/analytics.cljs
@@ -3,7 +3,7 @@
 (defn gtag
   "Call `gtag` with a given `info` (a clojure map with the info we want to track)"
   [info]
-  (js/gtag "event" "registered-user" (clj->js info)))
+  (js/gtag "event" "tool-usage" (clj->js info)))
 
 (defn gtag-tool-clicked
   "Call `gtag` for a specific togglable tool named `tool-name`
