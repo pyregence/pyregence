381b889a6166d88bf458de7a6be46a3e5f185d47
Daniel Silva <dsilva@sig-gis.com>
2025-04-03 16:02:00 -0300

    improve docs

--- src/cljs/pyregence/analytics.cljs
+++ src/cljs/pyregence/analytics.cljs
@@ -1,14 +1,15 @@
 (ns pyregence.analytics)
 
 (defn gtag
-  "Call `gtag` with a given `info` (a clojure map with the info we want to track)"
+  "Call `gtag` with a given `info` (a clojure map with the info we want to track)
+  `gtag` is a function provided by google analytics tool.
+   See https://developers.google.com/analytics/devguides/collection/ga4"
   [event-name info]
   (js/gtag "event" event-name (clj->js info)))
 
 (defn gtag-tool-clicked
-  "Call `gtag` for a specific togglable tool named `tool-name`
-  `gtag` is a function provided by google analytics tool.
-  See https://developers.google.com/analytics/devguides/collection/ga4"
+  "Call `gtag` passing the `tool-usage` event.
+   If the tool is toggable, `show?` it the current state."
   ([event-name]
    (gtag "tool-usage" {:tool-clicked event-name}))
   ([show? tool-name]
