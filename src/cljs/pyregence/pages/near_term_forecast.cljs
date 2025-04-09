ca39bb36079dbcce6b6a34653650b36b0b7e8478
Sif <sbiri@sig-gis.com>
2025-04-09 08:09:51 -0700

    PYR1-1069  Rename `dev-mode` `config.edn` key to `show-disclaimer` across Pyrecast (#932)
    * use show-disclaimer key
    
    * remove check for dev-mode in handlers
    
    * Fixing show disclaimer logic
    
    ---------
    
    Co-authored-by: Oliver-BE <obaldwinedwards@gmail.com>

--- src/cljs/pyregence/pages/near_term_forecast.cljs
+++ src/cljs/pyregence/pages/near_term_forecast.cljs
@@ -768,7 +768,7 @@
                :on-mouse-up   #(reset! mouse-down? false)}]))
 
 (defn- message-modal []
-  (r/with-let [show-me? (r/atom (not @!/dev-mode?))]
+  (r/with-let [show-me? (r/atom @!/show-disclaimer?)]
     (when @show-me?
       [:div#message-modal {:style ($/modal)}
        [:div {:style ($message-modal false)}
