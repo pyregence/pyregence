8539dc7fb3311917fb6632a899f05935cee914d5
drew verlee <drew.verlee@gmail.com>
2025-04-24 17:20:53 -0500

    In some database servers, like those on GCP, the Postgres user has
    no permissions so we have to grant them it.

--- src/sql/create_db.sql
+++ src/sql/create_db.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS pyregence;
 DROP ROLE IF EXISTS pyregence;
 CREATE ROLE pyregence WITH LOGIN CREATEDB PASSWORD 'pyregence';
+GRANT pyregence TO postgres;
+SET ROLE pyregence;
 CREATE DATABASE pyregence WITH OWNER pyregence;
 \c pyregence
 CREATE EXTENSION pgcrypto;
