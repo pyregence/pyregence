5f0fff5659c744864df6ef932b897c7047fbdc44
drew verlee <drew.verlee@gmail.com>
2025-05-30 14:51:30 -0500

    WIP working on creating an isolated pyregence env.

---
+++ manifest.scm
@@ -0,0 +1,11 @@
+(use-modules ((gnu packages) #:select (specifications->manifest)))
+
+(specifications->manifest
+ (list "clojure-tools@1.11" ;; to build and run clojure
+       "openjdk@21:jdk" ;; to build, to run clojure
+       "node@22"        ;; to build and run clojurescript
+       "git"       ;; source control 
+       "nss-certs" ;; for build certs
+       "bash"      ;; for sh (only for dev?)
+       "coreutils" ;; for mkdir which is used in build
+       ))
