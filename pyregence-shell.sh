47953a8d63eb7b4ced1ba3ab2e4b92715a95858a
drew verlee <drew.verlee@gmail.com>
2025-06-02 12:31:51 -0500

    I need to pass args to the script more then i need a fire emoji.

--- pyregence-shell.sh
+++ pyregence-shell.sh
@@ -2,13 +2,10 @@
 # Run to enter a container that contains all pyregences dependences (e.g java)
 
 guix time-machine \
-  --channels=channels.scm \
-  -- shell \
-  --manifest=manifest.scm \
-  --container \
-  --network \
-  -S /usr/bin/env=bin/env \
-  -- bash -c '
-  export PS1="🔥 \w \$ "
-  exec bash
-  '
+     --channels=channels.scm \
+     -- shell \
+     --manifest=manifest.scm \
+     --container \
+     --network \
+     -S /usr/bin/env=bin/env \
+     -- "$@"
