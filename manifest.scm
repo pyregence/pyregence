f459d396bd86794364bef5021a1009648d945231
drew verlee <drew.verlee@gmail.com>
2025-06-02 12:32:38 -0500

    Remove whitespace.

--- manifest.scm
+++ manifest.scm
@@ -4,7 +4,7 @@
  (list "clojure-tools@1.11" ;; to build and run clojure
        "openjdk@21:jdk" ;; to build, to run clojure
        "node@22"        ;; to build and run clojurescript
-       "git"       ;; source control 
+       "git"       ;; source control
        "nss-certs" ;; for build certs
        "bash"      ;; for sh (only for dev?)
        "coreutils" ;; for mkdir which is used in build
