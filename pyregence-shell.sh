a72fee38cbef7e078fd5e882ac2321547f047151
drew verlee <drew.verlee@gmail.com>
2025-06-03 10:51:32 -0500

    Share hosts m2 and gitlabs so deps can cache dependencies there.

--- pyregence-shell.sh
+++ pyregence-shell.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-# Run to enter a container that contains all pyregences dependences (e.g java)
+# Run to enter a shell that contains all pyregences dependences (e.g java)
+# Notes
+# + symlink to env because webpack looks for node there.
+# + share m2 and gitlabs for clojure deps caching.
 
 guix time-machine \
-     --channels=channels.scm \
-     -- shell \
-     --manifest=manifest.scm \
-     --container \
-     --network \
-     -S /usr/bin/env=bin/env \
-     -- "$@"
+         --channels=channels.scm \
+         -- shell \
+         --manifest=manifest.scm \
+         --container \
+         --network \
+         --symlink="/usr/bin/env=bin/env" \
+         --share=$HOME/.m2 \
+         --share=$HOME/.gitlibs \
+         "$@"
