8ca2dff0f8c709c1545fc8d1cd4b453dd98ea8e9
drew verlee <drew.verlee@gmail.com>
2025-06-05 15:51:35 -0500

    Clarify build notes.

--- manifest.scm
+++ manifest.scm
@@ -1,11 +1,11 @@
 (use-modules ((gnu packages) #:select (specifications->manifest)))
 
 (specifications->manifest
- (list "clojure-tools@1.11" ;; to build and run clojure
-       "openjdk@21:jdk" ;; to build, to run clojure
-       "node@22"        ;; to build and run clojurescript
-       "git"       ;; source control
-       "nss-certs" ;; for build certs
-       "bash"      ;; for sh (only for dev?)
-       "coreutils" ;; for mkdir which is used in build
+ (list "clojure-tools@1.11" ;; to build and run clojure.
+       "openjdk@21:jdk"     ;; to build and to run clojure.
+       "node@22"            ;; to build and run clojurescript.
+       "git"                ;; to build the jar we use git information.
+       "nss-certs"          ;; to build we need certs.
+       "bash"               ;; for sh (TODO double check this is needed...).
+       "coreutils"          ;; to build the jar it needs mkdir.
        ))
