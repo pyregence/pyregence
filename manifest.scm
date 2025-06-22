c1a24ee1e277b9848b8d9ae5fff63b8b8de2fce9
drew verlee <drew.verlee@gmail.com>
2025-06-22 16:07:24 -0500

    Clarify the usage of dependencies and update README.

--- manifest.scm
+++ manifest.scm
@@ -1,11 +1,11 @@
 (use-modules ((gnu packages) #:select (specifications->manifest)))
 
-(specifications->manifest
- (list "clojure-tools@1.11" ;; to build and run clojure.
-       "openjdk@21:jdk"     ;; to build and to run clojure.
-       "node@22"            ;; to build and run clojurescript.
-       "git"                ;; to build the jar we use git information.
-       "nss-certs"          ;; to build we need certs.
-       "bash"               ;; for sh (TODO double check this is needed...).
-       "coreutils"          ;; to build the jar it needs mkdir.
+(specifications->manifest   ;; build jar | run jar | run clojure
+ (list "clojure-tools@1.11" ;; T         | F       | T
+       "openjdk@21:jdk"     ;; T         | T       | T
+       "node@22"            ;; T         | F       | F
+       "git"                ;; T         | F       | F
+       "nss-certs"          ;; T         | F       | F
+       "bash"               ;; T (sh)    | F       | F
+       "coreutils"          ;; T (mdkir) | F       | F
        ))
