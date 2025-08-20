7c1dff9050771274122e1bd4b7298d8d502cd120
drew verlee <drew.verlee@gmail.com>
2025-08-20 09:25:37 -0500

    pull a sample of ws and show them on the screen.
    - need to refresh these periodically, maybe every 3 days, maybe using
    cache bust.

--- src/clj/pyregence/weather_stations.clj
+++ src/clj/pyregence/weather_stations.clj
@@ -1,148 +1,40 @@
 (ns pyregence.weather-stations
   (:require [clj-http.client :as client]))
-(def urls (atom []))
-(def cursors (atom []))
 
-;;Retrieve the metadata for your location from https://api.weather.gov/points/{lat},{lon}.
+;;TODO consider turning this isn't a transducer, that way i think limit and sleep wouldn't have to be passed in as arguments.
+;;NOTE this makes no attempt to gracefully handler failures
+;;TODO before release to prod we should see how this behaves in dev over time and potentially have it handle failures that come up!
+(defn get-observation-stations!
+  "Returns a list of observation stations from the weather.gov api which you can limit by total urls or fetches per second"
+  [& {:keys [total-station-limit rate-limit-per-second] :or {rate-limit-per-second 30 total-url-limit Double/POSITIVE_INFINITY}}]
+  (loop [url "https://api.weather.gov/stations"
+         observation-stations []]
+    (let [{{new-observation-stations        :features
+            {next-batch-of-stations-url :next} :pagination} :body
+           :as response} (client/get url {:as :json})
+          ;;NOTE it's unclear how to tell we reached the end as the next-batch-of-stations-url isn't nil even after it runs out.
+          ;;so we count the stations instead, which should be just as accurate.
+          no-new-observation-stations? (zero? (count new-observation-stations))
+          observation-stations (concat observation-stations new-observation-stations)
+          station-cap-hit? (<= total-station-limit (count observation-stations))]
+      (Thread/sleep (* rate-limit-per-second 1000))
+      (println (count observation-stations))
+      (if (or no-new-observation-stations? station-cap-hit?)
+        observation-stations
+        (recur next-batch-of-stations-url observation-stations)))))
 
-(defn get-stations-put-in-url
-  []
-  (loop [u "https://api.weather.gov/stations"]
-    (let [{{os        :observationStations
-            {p :next} :pagination} :body} (client/get u {:as :json})]
-      (swap! urls concat os)
-      (swap! cursors conj p)
-      (println (count os))
-      (println p)
-      (println (count @urls))
-      (Thread/sleep 5000)
-      (when-not (< 2000 (count @urls))
-        (recur p)))))
+(def observation-stations (get-observation-stations! {:total-station-limit 4000}))
 
-(comment
-  (get-stations-put-in-url))
-(defn get-some-observation-stations
-  []
-
-  (map
-    #(client/get % {:as :json})
-    (take 20 @urls)))
-
-(defn stations
-  []
-  (->> (get-some-observation-stations)
-       (map :body)
-       (map (fn [{{i :stationIdentifier n :name} :properties
-                  {c :coordinates}               :geometry}]
-              [i n c]))))
-
-(comment
-  (def s *1)
-
-  (map
-    (fn [[id name [lat long :as coordinates]]]
-      {:type "Feature"
-       :geometry {:type "Point" :coordinates coordinates}
-       :properties
-       })
-    s)
-
-
-  ;; TODO just use keys don't do [blah, foo]
-  ;; => (["340PG" "Road to Ranches" [-122.7331 38.05648]]
-  ;;     ["156SE" "SCE Bautista Creek" [-116.85673 33.70725]]
-  ;;     ["612SE" "SCE Rocky Court" [-118.34749 35.08966]]
-  ;;     ["049SE" "SCE Magic Mountain" [-118.37337 34.42503]]
-  ;;     ["026CE" "56 Shadow Creek Ranch" [-95.39775 29.5553]])
-
-  )
-(def observations (atom []))
-
-(defn another-get-observations-from-urls
-  []
-  (loop [surls (take 10 @urls)]
-    (when-let [s (first surls)]
-      (Thread/sleep 2000)
-      (println "GET " s)
-      (swap! observations conj (client/get s {:as :json}))
-      (recur (rest surls)))))
-
-(comment
-  (def station-url (first (stations "observationStations")))
-  (def station (:body (getj station-url)))
-
-  station
-  {"@context"
-   ["https://geojson.org/geojson-ld/geojson-context.jsonld"
-    {"s"                "https://schema.org/",
-     "bearing"          {"@type" "s:QuantitativeValue"},
-     "city"             "s:addressLocality",
-     "wx"               "https://api.weather.gov/ontology#",
-     "county"           {"@type" "@id"},
-     "geo"              "http://www.opengis.net/ont/geosparql#",
-     "forecastOffice"   {"@type" "@id"},
-     "@version"         "1.1",
-     "value"            {"@id" "s:value"},
-     "geometry"         {"@id" "s:GeoCoordinates", "@type" "geo:wktLiteral"},
-     "publicZone"       {"@type" "@id"},
-     "distance"         {"@id" "s:Distance", "@type" "s:QuantitativeValue"},
-     "@vocab"           "https://api.weather.gov/ontology#",
-     "state"            "s:addressRegion",
-     "unit"             "http://codes.wmo.int/common/unit/",
-     "forecastGridData" {"@type" "@id"},
-     "unitCode"         {"@id" "s:unitCode", "@type" "@id"}}],
-   "id"       "https://api.weather.gov/stations/0007W",
-   "type"     "Feature",
-   "geometry" {"type" "Point", "coordinates" [-84.1787 30.53099]},
-   "properties"
-   {"forecast"          "https://api.weather.gov/zones/forecast/FLZ017",
-    "@id"               "https://api.weather.gov/stations/0007W",
-    "elevation"         {"unitCode" "wmoUnit:m", "value" 49.0728},
-    "timeZone"          "America/New_York",
-    "county"            "https://api.weather.gov/zones/county/FLC073",
-    "stationIdentifier" "0007W",
-    "name"              "Montford Middle",
-    "fireWeatherZone"   "https://api.weather.gov/zones/fire/FLZ017",
-    "@type"             "wx:ObservationStation"}})
-
-(comment
-  (select-keys station ["properties" "geometry"]))
-
-(def url "https://api.weather.gov/stations/0007W/observations/latest")
-(defn getj [u] (client/get u {:as :json-string-keys}))
-(defn obs [] (get-in (getj url) [:body "properties"]))
-(comment
-  (select-keys (obs) ["windSpeed" "windDirection" "windGust" "temperature" "relativeHumidity" "dewpoint"])
-  {"windSpeed"
-   {"unitCode" "wmoUnit:km_h-1", "value" 8.028, "qualityControl" "V"},
-   "windDirection"
-   {"unitCode" "wmoUnit:degree_(angle)", "value" 323, "qualityControl" "V"},
-   "windGust"    {"unitCode" "wmoUnit:km_h-1", "value" nil, "qualityControl" "Z"},
-   "temperature" {"unitCode" "wmoUnit:degC", "value" nil, "qualityControl" "Z"},
-   "relativeHumidity"
-   {"unitCode" "wmoUnit:percent", "value" nil, "qualityControl" "Z"},
-   "dewpoint"    {"unitCode" "wmoUnit:degC", "value" 23.23, "qualityControl" "V"}})
-
-;; > The first two elements are longitude and latitude, or easting and northing,
-
-;; [longitude, latitude].
-;; ~ https://datatracker.ietf.org/doc/html/rfc7946
-
-;;["340PG" "Road to Ranches" [-122.7331 38.05648]]
+;;TODO rethink this idea because were changing the ws to be the the camera and it might not need
+;; everything like pan and tilt.
+(defn ws->camera-format
+  [{{[lon lat] :coordinates} :geometry :as ws}]
+  (-> ws
+      (update :properties select-keys [:name])
+      (update :properties assoc :longitude lon :latitude lat)))
 
 (defn get-weather-stations
   [_]
   {:type "FeatureCollection"
-   :features [{:type     "Feature",
-               :geometry {:type "Point", :coordinates [-84.1787 30.53099] },
-               :properties
-               {#_#_:image-url
-                "https://prod.weathernode.net/data/img/15794/2025/08/15/Hogsback_1_1755288466_5020.jpg",
-                #_#_:pan         32.4,
-                :name        "Montford middle",
-                #_#_:update-time "2025-08-15T20:07:46Z",
-                #_#_:tilt        0.24,
-                :longitude   -84.1787,
-                #_#_:state       "CA",
-                #_#_:api-name    "alert-west",
-                :latitude    30.53099}}]})
+   :features (->> observation-stations
+                  (map ws->camera-format))})
