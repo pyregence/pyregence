da488cb826b1c70c3f8c5ea3394ce43928acf9ca
drew verlee <drew.verlee@gmail.com>
2025-08-21 09:39:26 -0500

    simplify get-stations fn and consider triggers: time vs event.

--- src/clj/pyregence/weather_stations.clj
+++ src/clj/pyregence/weather_stations.clj
@@ -6,42 +6,26 @@
 ;;TODO consider turning this isn't a transducer, that way i think limit and sleep wouldn't have to be passed in as arguments.
 ;;TODO before release to prod we should see how this behaves in dev over time and potentially have it handle failures that come up!
 ;;TODO consider adding a connect-timeout and socket-timeout
-;;TODO concerning adding something to check if this timesout, returns a none 200, doesn't reach the end, in some
-;;set amount of time.
-;; that takes roughly an hour
+;;TODO concerning adding something to check if this timesout, returns a none 200, doesn't reach the end, in some set amount of time.
 ;;TODO consider handling 504/timeout
-;;TODO consider having this be async...
+;;TODO consider having this be clj-async...
 (defn get-observation-stations!
-  "Returns a list of observation stations from the weather.gov api which you can limit by total urls or fetches per second."
-  [& {:keys [total-station-limit rate-limit-per-second]
-      :or {rate-limit-per-second 10 total-station-limit Double/POSITIVE_INFINITY connection-timeout 10}}]
-  (loop [url "https://api.weather.gov/stations"
-         observation-stations []]
-    (let [{{new-observation-stations        :features
-            {next-batch-of-stations-url :next} :pagination} :body
-           status :status
-           :as response} (client/get url {:as :json
-                                          :headers {"User-Agent" "support@sig-gis.com"}})
-          no-new-observation-stations? (zero? (count new-observation-stations))
-          observation-stations (concat observation-stations new-observation-stations)
-          station-cap-hit? (<= total-station-limit (count observation-stations))]
-      (Thread/sleep (* rate-limit-per-second 1000))
-      (if (or no-new-observation-stations? station-cap-hit?)
-        observation-stations
-        (recur next-batch-of-stations-url observation-stations)))))
+  []
+  (loop [url "https://api.weather.gov/stations"]
+    (let [{{new-observation-stations           :features
+            {next-batch-of-stations-url :next} :pagination} :body}
+          (client/get url {:as      :json
+                           :headers {"User-Agent" "support@sig-gis.com"}})]
+      (when (seq new-observation-stations)
+        observation-stations (swap! observation-stations concat new-observation-stations)
+        (Thread/sleep 2000)
+        (recur next-batch-of-stations-url)))))
 
 ;;TODO this needs a way to refresh the observation-stations atom with new data on some trigger: time or user event
-(defonce f
-  (future
-    (loop [url "https://api.weather.gov/stations"]
-      (let [{{new-observation-stations           :features
-              {next-batch-of-stations-url :next} :pagination} :body}
-            (client/get url {:as      :json
-                             :headers {"User-Agent" "support@sig-gis.com"}})]
-        (when (seq new-observation-stations)
-          observation-stations (swap! observation-stations concat new-observation-stations)
-          (Thread/sleep 2000)
-          (recur next-batch-of-stations-url))))))
+;; If it's a time based event this defonce to future layer might be ok, if user-event this logic will obviously move.
+;; we have an example of user-event based cache refresh in camera.
+(defonce get-observation-stations-in-the-background!
+  (future (get-observation-stations!)))
 
 ;; example time based trigger
 (comment
