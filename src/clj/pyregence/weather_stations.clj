8449f982a1b98e7e7c8d99ca987e4293765e181a
drew verlee <drew.verlee@gmail.com>
2025-08-21 09:29:16 -0500

    Adding a User-Agent might have have 'fixed' apis rate-limiting.
    That or it's because i ran it at night...

--- src/clj/pyregence/weather_stations.clj
+++ src/clj/pyregence/weather_stations.clj
@@ -1,43 +1,71 @@
 (ns pyregence.weather-stations
   (:require [clj-http.client :as client]))
 
+(defonce observation-stations (atom []))
+
 ;;TODO consider turning this isn't a transducer, that way i think limit and sleep wouldn't have to be passed in as arguments.
-;;NOTE this makes no attempt to gracefully handler failures
 ;;TODO before release to prod we should see how this behaves in dev over time and potentially have it handle failures that come up!
 ;;TODO consider adding a connect-timeout and socket-timeout
-;;TODO add something to check if this timesout, returns a none 200, doesn't reach the end, in some
+;;TODO concerning adding something to check if this timesout, returns a none 200, doesn't reach the end, in some
 ;;set amount of time.
-;;NOTE there are roughly 63500 stations at 30 seconds, which is a safe pull-rate given the implicit rate limit,
 ;; that takes roughly an hour
 ;;TODO consider handling 504/timeout
+;;TODO consider having this be async...
 (defn get-observation-stations!
   "Returns a list of observation stations from the weather.gov api which you can limit by total urls or fetches per second."
-  [& {:keys [total-station-limit rate-limit-per-second] :or {rate-limit-per-second 30 total-station-limit Double/POSITIVE_INFINITY}}]
-  (println "start---")
+  [& {:keys [total-station-limit rate-limit-per-second]
+      :or {rate-limit-per-second 10 total-station-limit Double/POSITIVE_INFINITY connection-timeout 10}}]
   (loop [url "https://api.weather.gov/stations"
          observation-stations []]
     (let [{{new-observation-stations        :features
             {next-batch-of-stations-url :next} :pagination} :body
+           status :status
            :as response} (client/get url {:as :json
                                           :headers {"User-Agent" "support@sig-gis.com"}})
-          ;;NOTE it's unclear how to tell we reached the end as the next-batch-of-stations-url isn't nil even after it runs out.
-          ;;so we count the stations instead, which should be just as accurate.
           no-new-observation-stations? (zero? (count new-observation-stations))
           observation-stations (concat observation-stations new-observation-stations)
           station-cap-hit? (<= total-station-limit (count observation-stations))]
-      (println {:status (:status response) :count (count observation-stations) })
       (Thread/sleep (* rate-limit-per-second 1000))
       (if (or no-new-observation-stations? station-cap-hit?)
         observation-stations
         (recur next-batch-of-stations-url observation-stations)))))
 
-;;(defonce observation-stations (get-observation-stations! {:total-station-limit 4000}))
-
-
-(def observation-stations
+;;TODO this needs a way to refresh the observation-stations atom with new data on some trigger: time or user event
+(defonce f
   (future
-    (time (get-observation-stations! {:rate-limit-per-second 120}))))
+    (loop [url "https://api.weather.gov/stations"]
+      (let [{{new-observation-stations           :features
+              {next-batch-of-stations-url :next} :pagination} :body}
+            (client/get url {:as      :json
+                             :headers {"User-Agent" "support@sig-gis.com"}})]
+        (when (seq new-observation-stations)
+          observation-stations (swap! observation-stations concat new-observation-stations)
+          (Thread/sleep 2000)
+          (recur next-batch-of-stations-url))))))
 
+;; example time based trigger
+(comment
+  (defonce counter (atom 0))
+
+  (def scheduled-counter
+    (future
+      (loop [n 0]
+        (Thread/sleep (* 2000 ;; 1s
+                         ;;60   ;; 1m
+                         ;;60   ;; 1h
+                         ;;24   ;; 1d
+                         ;;7    ;; 1w
+                         )    ;; => 604800000
+                      )
+
+        (recur (reset! counter (inc n))))))
+
+  (def scheduled-counter
+    (future
+      (loop [n 0]
+        (Thread/sleep 2000)
+        (recur (reset! counter (inc n))))))
+  )
 
 ;;TODO rethink this idea because were changing the ws to be the the camera and it might not need
 ;; everything like pan and tilt.
@@ -50,5 +78,5 @@
 (defn get-weather-stations
   [_]
   {:type "FeatureCollection"
-   :features [] #_(->> observation-stations
+   :features (->> @observation-stations
                   (map ws->camera-format))})
