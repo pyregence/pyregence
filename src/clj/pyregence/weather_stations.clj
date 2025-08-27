810556a14de268f6e134fc4370327d03cad06c4e
drew verlee <drew.verlee@gmail.com>
2025-08-26 23:54:59 -0500

    Run get-observations as part of starting the server.
    This avoids having it run when the code is compiled.

--- src/clj/pyregence/weather_stations.clj
+++ src/clj/pyregence/weather_stations.clj
@@ -1,82 +1,40 @@
 (ns pyregence.weather-stations
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client     :as client]
+            [triangulum.logging  :refer [log]]))
 
 (defonce observation-stations (atom []))
 
-;;TODO consider turning this isn't a transducer, that way i think limit and sleep wouldn't have to be passed in as arguments.
-;;TODO before release to prod we should see how this behaves in dev over time and potentially have it handle failures that come up!
-;;TODO consider adding a connect-timeout and socket-timeout
-;;TODO concerning adding something to check if this timesout, returns a none 200, doesn't reach the end, in some set amount of time.
-;;TODO consider handling 504/timeout
-;;TODO consider having this be clj-async...
 (defn get-observation-stations!
   []
-  (loop [url "https://api.weather.gov/stations"]
+  (loop [url "https://api.weather.gov/stations"
+         observation-stations []]
+    (Thread/sleep 2000)
     (let [{{new-observation-stations           :features
             {next-batch-of-stations-url :next} :pagination} :body}
           (client/get url {:as      :json
                            :headers {"User-Agent" "support@sig-gis.com"}
                            ;; throw if we don't hear back in 3 minutes, because by then we never will.
                            :connection-timeout (* 1000 60 3)})]
-      (when (seq new-observation-stations)
-        (swap! observation-stations concat new-observation-stations)
-        (Thread/sleep 2000)
-        (recur next-batch-of-stations-url)))))
+      (if (seq new-observation-stations)
+        (recur next-batch-of-stations-url (concat observation-stations new-observation-stations))
+        observation-stations))))
 
-;;TODO this needs a way to refresh the observation-stations atom with new data on some trigger: time or user event
-;; If it's a time based event this defonce to future layer might be ok, if user-event this logic will obviously move.
-;; we have an example of user-event based cache refresh in camera.
-(defonce get-observation-stations-in-the-background!
+(defn periodically-get-observation-stations-in-the-background!
+  []
   (future
-    (get-observation-stations!)))
+    (loop []
+      (try
+        (reset! observation-stations (get-observation-stations!))
+        (catch Exception ex (log (ex-data ex) :truncate? false)))
+      (Thread/sleep (* 1000 ;; 1s
+                       60   ;; 1m
+                       5   ;;  5m
+                     ;;24   ;; 1 day
+                     ;;2    ;; 2 days
+                       ))
+      (recur))))
 
-;; example time based trigger
-(comment
-  (defonce counter (atom 0))
-
-  (def scheduled-counter
-    (future
-      (loop []
-        (println @counter)
-        (throw (AssertionError. "wrong"))
-        (Thread/sleep (* 1000 ;; 1s
-                         ;;60   ;; 1m
-                         ;;60   ;; 1h
-                         ;;24   ;; 1d
-                         ;;2    ;; s days
-                         ))
-        (swap! counter inc)
-        (recur))))
-
-  )
-
-;; What the observations logic will end up looking like.
-;;TODO consider saving if get-observations-stations succeeded... as in we reached the end
-;;the issue is, we would have to log if it didn't, then alert on that log... and that would have to be on
-;;a time schedule..
-(comment
-  (require '[triangulum.logging  :refer [log]])
-
-  (((defonce get-observation-stations-in-the-background!
-      (future
-        (loop []
-
-          (try
-            (get-observation-stations!)
-            (catch Exception ex (log (ex-data ex) :truncate? false)))
-          (Thread/sleep (* 1000 ;; 1s
-                           60   ;; 1m
-                           60   ;; 1h
-                           24   ;; 1d
-                           2    ;; s days
-                           ))
-          (recur))))))
-  )
-
-
-;;TODO rethink this idea because were changing the ws to be the the camera and it might not need
-;; everything like pan and tilt.
-(defn ws->camera-format
+(defn select-relevent-properties
   [{{[lon lat] :coordinates} :geometry :as ws}]
   (-> ws
       (update :properties select-keys [:name :stationIdentifier])
@@ -86,4 +44,4 @@
   [_]
   {:type "FeatureCollection"
    :features (->> @observation-stations
-                  (map ws->camera-format))})
+                  (map select-relevent-properties))})
