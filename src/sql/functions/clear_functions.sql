c361de480b6d2455df280a213c85e7bd8fd93ed6
Gary Johnson <lambdatronic@disroot.org>
2025-10-03 23:47:00 +0700

    Changed clear_functions.sql to drop triggers before dropping functions.

--- src/sql/functions/clear_functions.sql
+++ src/sql/functions/clear_functions.sql
@@ -1,20 +1,9 @@
 -- NAMESPACE: clear
 
--- Drop all functions owned by the current user
-WITH funcs AS (
-  SELECT
-    format('DROP FUNCTION %s CASCADE;', p.oid::regprocedure) AS drop_statement
-  FROM pg_proc p
-  JOIN pg_roles r ON p.proowner = r.oid
-  WHERE r.rolname = :'user'
-    AND p.prokind = 'f'
-)
-SELECT drop_statement FROM funcs \gexec
-
 -- Drop all triggers owned by the current user
 WITH triggers AS (
   SELECT DISTINCT
-    format('DROP TRIGGER IF EXISTS %I ON %I.%I RESTRICT;',
+    format('DROP TRIGGER IF EXISTS %I ON %I.%I;',
            t.tgname, 
            n.nspname, 
            c.relname) AS drop_statement
@@ -27,3 +16,14 @@ WITH triggers AS (
     AND NOT t.tgisinternal
 )
 SELECT drop_statement FROM triggers \gexec
+
+-- Drop all functions owned by the current user
+WITH funcs AS (
+  SELECT
+    format('DROP FUNCTION %s CASCADE;', p.oid::regprocedure) AS drop_statement
+  FROM pg_proc p
+  JOIN pg_roles r ON p.proowner = r.oid
+  WHERE r.rolname = :'user'
+    AND p.prokind = 'f'
+)
+SELECT drop_statement FROM funcs \gexec
