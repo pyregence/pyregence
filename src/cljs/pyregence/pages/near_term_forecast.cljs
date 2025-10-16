0030e5922522d62fd44b6503fb3e043549882263
drew verlee <drew.verlee@gmail.com>
2025-10-16 10:49:16 -0500

    Allow message modal (w/ TOS accept) to take up 100% parent.
    This fixes an issue where the TOS acceptance wasn't visable on mobile, and
    I think it works because ... without this the accept section is competing with the
    Pyrecast header for space.

--- src/cljs/pyregence/pages/near_term_forecast.cljs
+++ src/cljs/pyregence/pages/near_term_forecast.cljs
@@ -814,7 +814,7 @@
           (and usage-terms-and-conditions-date (not= usage-terms-and-conditions-date-local-storage-mirror usage-terms-and-conditions-date))]
 
       (when not-accepted-current-usage-terms-and-conditions?
-        [:div#message-modal {:style ($/modal)}
+        [:div#message-modal {:style (assoc ($/modal) :height "100%")}
          [:div {:style ($message-modal false)}
           [:div
            [:div {:style {:background ($/color-picker :yellow)
