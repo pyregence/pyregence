0dc8d87b3f398fad2c09369c294ea6beda99c787
drew verlee <drew.verlee@gmail.com>
2025-10-20 15:17:05 -0500

    put svg alongside others.

--- src/cljs/pyregence/components/settings/nav_bar.cljs
+++ src/cljs/pyregence/components/settings/nav_bar.cljs
@@ -15,31 +15,6 @@
      :grid-template-areas "\"banner banner\" \"header header\" \"nav body\""}}
    [:div {:style {:grid-area "banner"}}]])
 
-;;TODO move to svg ns, its not there bc it was throwing errors there...
-(defn logout
-  [& {:keys [height width]}]
-  [:svg
-   {:xmlns "http://www.w3.org/2000/svg",
-    :width width,
-    :height height,
-    :viewBox "0 0 24 24",
-    :fill "none"}
-   [:mask
-    {:id "mask0_228_3867",
-     :style {:mask-type "alpha"},
-     :maskunits "userSpaceOnUse",
-     :x "0",
-     :y "0",
-     :width "24",
-     :height "24"}
-    [:rect {:width "24", :height "24", :fill "#D9D9D9"}]]
-   [:g
-    {:mask "url(#mask0_228_3867)"}
-    [:path
-     {:d
-      "M5 21C4.45 21 3.97917 20.8042 3.5875 20.4125C3.19583 20.0208 3 19.55 3 19V5C3 4.45 3.19583 3.97917 3.5875 3.5875C3.97917 3.19583 4.45 3 5 3H12V5H5V19H12V21H5ZM16 17L14.625 15.55L17.175 13H9V11H17.175L14.625 8.45L16 7L21 12L16 17Z",
-      :fill "black"}]]])
-
 (defn button
   [{:keys [icon text tag on-click selected?]}]
   [tag {:on-click on-click
@@ -126,4 +101,4 @@
                        :border-top "1px solid #E1E1E1"
                        :border-bottom "1px solid #E1E1E1"}}
     [settings]]
-   [button {:text "Logout" :icon logout :tag :logout}]])
+   [button {:text "Logout" :icon svg/logout :tag :logout}]])
