91304c1d2537a085841fcc7e2aadee45e03f1165
drew verlee <drew.verlee@gmail.com>
2025-10-28 11:27:36 -0500

    parameterize last updated password.

--- src/cljs/pyregence/components/settings/body.cljs
+++ src/cljs/pyregence/components/settings/body.cljs
@@ -125,7 +125,7 @@
      [:TODO-BUTTON "TODO SAVE NAME BUTTON"]]))
 
 (defn main
-  []
+  [{:keys [last-updated-password]}]
   [:div {:style {:display        "flex"
                  :height         "942px"
                  :flex-direction "column"
@@ -154,7 +154,7 @@
                           :width           "100%"}}
             [:p {:style {:margin "0px"}} "TODO SAVE BUTTON"]
             [input-show {:label "Last Updated"
-                         :text  "11/16/2024"}]]]}]
+                         :text last-updated-password}]]]}]
    [card {:title "NOTIFICATION PREFERENCES"
           :children
           [:<>
