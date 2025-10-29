67e3c7fd7ede038cf67a126e47a2f5285cba13ae
drew verlee <drew.verlee@gmail.com>
2025-10-29 14:58:33 -0500

    Add a mock nav bar.

--- src/cljs/pyregence/pages/account_settings.cljs
+++ src/cljs/pyregence/pages/account_settings.cljs
@@ -1,14 +1,20 @@
-(ns pyregence.pages.account-settings)
+(ns pyregence.pages.account-settings
+  (:require
+   [pyregence.styles :as $]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; UI Components
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+;; TODO $nav-bar was copied from the nav_bar.cljs to mock this page out, this page will
+;; need, according to the designs, the forecast tabs and the new settings tabs.
+(defn- $nav-bar
+  []
+  {:display         "flex"
+   :justify-content "center"
+   :align-items     "center"
+   :width           "100%"
+   ;;TODO added height
+   :height "33px"})
 
 (defn root-component
   "The root component of the /account-settings page."
-  [_]
-  [:div {:id "help-page"}
-   [:h1 {:style {:margin     "5rem 2.5rem"
-                 :text-align "center"}}
-    "This page is under construction. Please check back later!"]])
+  []
+  ;;TODO add settings components
+  [:nav  {:style ($/combine $nav-bar {:background ($/color-picker :yellow)})}])
