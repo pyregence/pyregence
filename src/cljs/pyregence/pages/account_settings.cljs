3cde98ccdc2dd2e589477d443e4a03030fcc924a
drew verlee <drew.verlee@gmail.com>
2025-11-04 21:46:09 -0600

    Only call the backend if we don't have orgs.
    The other code formed a loop because it kept re-triggering the backend call.

--- src/cljs/pyregence/pages/account_settings.cljs
+++ src/cljs/pyregence/pages/account_settings.cljs
@@ -9,34 +9,6 @@
    [reagent.core                          :as r]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Functions copied and state logic copied from admin.cljs
-;; TODO these should be shared with admin.cljs unless admin is going away.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defonce ^{:doc "Vector of the user's organizations."}
- *orgs (r/atom []))
-
-;;TODO use pending-get-organizations or remove it.
-(defonce ^{:doc "The pending state of a organization list request."}
- pending-get-organizations? (r/atom false))
-
-;; mostly copied from admin, some small alterations, didn't need to set state in the same way...
-(defn- get-organizations [user-role]
-  (reset! pending-get-organizations? true)
-  (go
-    ;;TODO double check the security aspect of this... is get-all-organizations
-    ;; some how more open then it should be?
-    (let [api-route (if (= user-role "super_admin")
-                      "get-all-organizations"
-                      "get-current-user-organization")
-          response  (<! (u-async/call-clj-async! api-route))]
-      (reset! *orgs (if (:success response)
-                      (->> (:body response)
-                           (edn/read-string))
-                      []))
-      (reset! pending-get-organizations? false))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Root component
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -47,31 +19,45 @@
   ;; call floating at the top, but how else to organize it?
   ;; TODO get-organizations could happen just when they click
   ;;organization settings, but then that would be slower...
-  (get-organizations user-role)
-  [:div {:style {:height         "100%"
-                 :display        "flex"
-                 :flex-direction "column"
-                 :font-family    "Roboto"}}
+  (r/with-let [orgs (r/atom [])]
+    ;; TODO this conditional check on @orgs prevents repeating calls to the backend
+    ;; But it might not make sense if we need to update orgs at other points. Consider other ways.
+    (when-not @orgs
+      (go
+        ;;TODO double check the security aspect of this... is get-all-organizations
+        ;; some how more open then it should be?
+        (let [api-route (if (= user-role "super_admin")
+                          "get-all-organizations"
+                          "get-current-user-organization")
+              response  (<! (u-async/call-clj-async! api-route))]
+          (reset! orgs (if (:success response)
+                         (->> (:body response)
+                              (edn/read-string))
+                         [])))))
+    [:div {:style {:height         "100%"
+                   :display        "flex"
+                   :flex-direction "column"
+                   :font-family    "Roboto"}}
    ;; TODO replace with actual upper nav bar
-   [:nav  {:style {:display         "flex"
-                   :justify-content "center"
-                   :align-items     "center"
-                   :width           "100%"
-                   :height          "33px"
-                   :background      ($/color-picker :yellow)}} "mock nav"]
-   (r/with-let [selected-log (r/atom [:account-settings])]
-     [:div {:style {:display        "flex"
-                    :flex-direction "row"
-                    :height         "100%"
-                    :background     ($/color-picker :lighter-gray)}}
-      [nav-bar/main {:selected-log  selected-log
-                     :organizations (mapv :org-name @*orgs)
-                     :user-role     user-role}]
-      (case (last @selected-log)
-        :account-settings
+     [:nav  {:style {:display         "flex"
+                     :justify-content "center"
+                     :align-items     "center"
+                     :width           "100%"
+                     :height          "33px"
+                     :background      ($/color-picker :yellow)}} "mock nav"]
+     (r/with-let [selected-log (r/atom [:account-settings])]
+       [:div {:style {:display        "flex"
+                      :flex-direction "row"
+                      :height         "100%"
+                      :background     ($/color-picker :lighter-gray)}}
+        [nav-bar/main {:selected-log  selected-log
+                       :organizations (mapv :org-name @orgs)
+                       :user-role     user-role}]
+        (case (last @selected-log)
+          :account-settings
         ;;TODO consider just passing args through, not renaming or re-mapping.
-        [body/main {:password-set-date "1/2/2020"
-                    :email-address     user-email
-                    :role-type         user-role
-                    :user-name         user-name}]
-        [:p "TODO"])])])
+          [body/main {:password-set-date "1/2/2020"
+                      :email-address     user-email
+                      :role-type         user-role
+                      :user-name         user-name}]
+          [:p "TODO"])])]))
