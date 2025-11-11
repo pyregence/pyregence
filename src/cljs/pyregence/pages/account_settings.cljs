8f8915eeede50dbaef7b4de3b480cec0324fad66
drew verlee <drew.verlee@gmail.com>
2025-11-11 11:00:40 -0600

    Use form 3 component (#992)
    * Add blank account settings page.
    
    * Add a mock nav bar.
    
    * Wire together existing /account-settings components.
    
    * Set settings page to use font family Roboto
    
    * use longer name bc react was complaining.
    
    * remove empty tabs that are conditioned out.
    
    * Fetch organizations and conditional change nav bar tabs.
    
    * Expose user name on settings page.
    
    By returning it in the verify_user_login sql fn which has comment that implies
    it should be returning the name anyway, and then passing it to the client session.
    
    * Small alterations to pass name on.
    
    * prevent box from looking to long.
    
    * no longer need name-part if using full name.
    
    * Clicking log out now actually logs you out!
    
    * commented out because component isn't ready.
    
    * Remove old comment.
    
    * format role type.
    
    * Hopefully improved grammar.
    
    * Use full name.
    
    * refactor.
    
    * remove unused code.
    
    * Route the user name from the db to the settings page.
    
    As the TODO notes, we copied a function from admin we should share or consolidate.
    Also, it seems like sql `verify_user_email` and `set_user_password` implicitly
    share a return table, is there a better way to sync their return values?
    
    * Only call the backend if we don't have orgs.
    
    The other code formed a loop because it kept re-triggering the backend call.
    
    * Starting state needs to be nil to trigger initial call.
    
    * Add padding to search icon.
    
    * Add TODO about adding on-click event.
    
    * comment out info tab until we have associated text.
    
    * Consider having a namespace that handles all things roles.
    
    * Let button grow taller on collapse.
    
    * Give some space between elements.
    
    * The set height seems more wrong then right.
    
    * use correct role name and add auth notes.
    
    * Only account manager and super admin can see unaffiliated members tab.
    
    * turn into a function so it's easier to test.
    
    * make fn private because it probably wont be used elsewhere...
    
    * improve doc and clean up.
    
    * formatting.
    
    * Clean up, clarify, and grammar improvements.
    
    * The `Roboto` font-family cascades down from the page settings.
    
    And we also remove some defaults.
    
    * quick fixes.
    
    * WIP this doesnt render the orgs, their fetched, but the component
    
    isn't updated.
    
    * print orgs.
    
    * wip debug notes.
    
    * I believe we need to call [wrap-page rather then (wrap-page...
    
    * Correctly use react components.
    
    In general we need to call [cmpt, not (cmpt, e.g not (wrap-page
    or (wrap-component. Because [cmpt allows for reagent to enact the react
    lifecycle, e.g passing it's props to `:reagent-render`.
    
    This change, from ( to [, triggers an issue because,
    some root-component, wrapped over `params` (should be called args) and
    added the `forecast-type`. So to re-produce that, we, as it idiomatic in clojure,
    bundled the root-component and the forecast-type into a hashmap and moved data
    in and out of it.
    
    This isn't pretty, I believe whats out of sync here is that there are really
    only to components that need this, short-term-forecast and long-term-forecast.
    
    So then, i think we should just make those component, and those stateful
    components, should be aware of their stateful differences.
    
    Basically, i propose that react state is  best composed through react
    components, not through clojure closures.
    
    * Reorganize the routing info to have less indirection and bindings.

--- src/cljs/pyregence/pages/account_settings.cljs
+++ src/cljs/pyregence/pages/account_settings.cljs
@@ -14,59 +14,55 @@
 
 (defn root-component
   "The root component of the /account-settings page."
-  [{:keys [user-role user-email user-name]}]
-  ;; TODO it feels awkward to just have this get-organizations api
-  ;; call floating at the top, but how else to organize it?
-  ;; TODO get-organizations could happen just when they click
-  ;;organization settings, but then that would be slower...
-  (r/with-let [orgs (r/atom nil)
-               selected-log (r/atom [:account-settings])]
-    ;; TODO this conditional check on @orgs prevents repeating calls to the backend
-    ;; But it might not make sense if we need to update orgs at other points. Consider other ways.
-    (when-not @orgs
-      (go
-        ;;TODO double check the security aspect of this... is get-all-organizations
-        ;; some how more open then it should be?
-        (let [api-route (if (= user-role "super_admin")
-                          "get-all-organizations"
-                          "get-current-user-organization")
-              response  (<! (u-async/call-clj-async! api-route))]
-          (reset! orgs (if (:success response)
-                         (->> (:body response)
-                              (edn/read-string))
-                         [])))))
-    [:div {:style {:height         "100%"
-                   :display        "flex"
-                   :flex-direction "column"
-                   :font-family    "Roboto"}}
-   ;; TODO replace with actual upper nav bar
-     [:nav  {:style {:display         "flex"
-                     :justify-content "center"
-                     :align-items     "center"
-                     :width           "100%"
-                     :height          "33px"
-                     :background      ($/color-picker :yellow)}} "mock nav"]
-     (let [tabs     (nav-bar/tab-data->tabs
-                      {:selected-log  selected-log
-                       :organizations (mapv :org-name @orgs)
-                       :user-role     user-role})
-           selected->tab-id (fn [selected]
-                              (:id (first ((group-by :selected? tabs) selected))))
-           selected (-> @selected-log last)
-           selected-page (selected->tab-id selected)]
-       [:div {:style {:display        "flex"
-                      :flex-direction "row"
-                      :height         "100%"
-                      :background     ($/color-picker :lighter-gray)}}
-        [nav-bar/main tabs]
-        (case selected-page
-          :account-settings
-          [body/main {:password-set-date "1/2/2020"
-                      :email-address     user-email
-                      :role-type         user-role
-                      :user-name         user-name}]
-          :organization-settings
-          [:p selected]
-          :unaffilated-members
-          [:p selected]
-          [:p "Page Not Found"])])]))
+  [{:keys [user-role]}]
+  (let [orgs (r/atom nil)
+        selected-log (r/atom [:account-settings])]
+    (r/create-class
+     {:display-name "account-sttings"
+      :component-did-mount
+      #(go
+          (let [api-route (if (= user-role "super_admin")
+                            "get-all-organizations"
+                            "get-current-user-organization")
+                response  (<! (u-async/call-clj-async! api-route))]
+            (reset! orgs (if (:success response)
+                           (->> (:body response)
+                                (edn/read-string))
+                           []))))
+      :reagent-render
+      (fn [{:keys [user-role user-email user-name]}]
+        [:div {:style {:height         "100%"
+                       :display        "flex"
+                       :flex-direction "column"
+                       :font-family    "Roboto"}}
+        ;; TODO replace with actual upper nav bar
+         [:nav  {:style {:display         "flex"
+                         :justify-content "center"
+                         :align-items     "center"
+                         :width           "100%"
+                         :height          "33px"
+                         :background      ($/color-picker :yellow)}} "mock nav"]
+         (let [tabs     (nav-bar/tab-data->tabs
+                         {:selected-log  selected-log
+                          :organizations (mapv :org-name @orgs)
+                          :user-role     user-role})
+               selected->tab-id (fn [selected]
+                                  (:id (first ((group-by :selected? tabs) selected))))
+               selected (-> @selected-log last)
+               selected-page (selected->tab-id selected)]
+           [:div {:style {:display        "flex"
+                          :flex-direction "row"
+                          :height         "100%"
+                          :background     ($/color-picker :lighter-gray)}}
+            [nav-bar/main tabs]
+            (case selected-page
+              :account-settings
+              [body/main {:password-set-date "1/2/2020"
+                          :email-address     user-email
+                          :role-type         user-role
+                          :user-name         user-name}]
+              :organization-settings
+              [:p selected]
+              :unaffilated-members
+              [:p selected]
+              [:p "Page Not Found"])])])})))
