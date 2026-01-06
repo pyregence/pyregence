6a5e721ef3513a9cdaeeecf90a2e77f9c53180ad
drew verlee <drew.verlee@gmail.com>
2026-01-06 12:51:23 -0600

    add back in missing feature highlight.

--- src/cljs/pyregence/components/map_controls/system_assets.cljs
+++ src/cljs/pyregence/components/map_controls/system_assets.cljs
@@ -18,7 +18,7 @@
                                  {:device "substation" :color "blue"}
                                  {:device "recloser" :color "green"}
                                  {:device "breaker" :color "orange"}]
-        ->id (fn [org_unique_id device] (str org_unique_id "-" device))]
+        ->id                    (fn [org_unique_id device] (str org_unique_id "-" device))]
     (r/create-class
      {:component-did-mount
       #(go
@@ -29,10 +29,10 @@
                                    (<! (u-async/call-clj-async! "get-orgs-with-system-assets"))]
                                (when success (edn/read-string body)))))]
            (doseq [{:keys [org_unique_id]} orgs-with-system-assets
-                   {:keys [device color]} device-info
-                   :let [id (->id org_unique_id device)
-                         source-layer (str org_unique_id "-devices")
-                         layer-name (str "psps-static_" org_unique_id "%3A" source-layer)]]
+                   {:keys [device color]}  device-info
+                   :let                    [id (->id org_unique_id device)
+                                            source-layer (str org_unique_id "-devices")
+                                            layer-name (str "psps-static_" org_unique_id "%3A" source-layer)]]
 
              (when-not (.getSource @mb/the-map id)
                (.addSource @mb/the-map id (clj->js (mb/mvt-source layer-name "psps"))))
@@ -44,11 +44,23 @@
                                                 :filter       ["==" ["get" "DVC_TYPE"] device]
                                                 :type         "circle"
                                                 :paint        {:circle-radius 6 :circle-color color}})))
-             (mb/set-visible-by-title! id false))))
+
+             (mb/set-visible-by-title! id false)
+             (mb/add-feature-highlight! id id
+                                        :click-fn
+                                        (fn [feature lnglat]
+                                          (mb/init-popup! "system-assets" lnglat
+                                                          [popup
+                                                           (let [{:keys [DVC_ID DVC_TYPE]}
+                                                                 (js->clj (gobj/get feature "properties")
+                                                                          :keywordize-keys true)]
+                                                             {:header  DVC_ID
+                                                              :options [{:label "Device Type" :value DVC_TYPE}]})]
+                                                          {:width "200px"}))
+                                        :source-layer source-layer))))
 
       :reagent-render
       #(when-let [orgs-with-system-assets (seq @orgs-with-system-assets)]
-         #_(print-layers)
          [u/collapsible-panel-section
           "System Assets"
           [:<>
