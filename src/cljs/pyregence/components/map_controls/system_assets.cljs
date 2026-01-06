610e43e58992932ed3e4632f26c05222fe7785e2
drew verlee <drew.verlee@gmail.com>
2026-01-06 11:20:53 -0600

    Try using a callback, this also doesn't work.

--- src/cljs/pyregence/components/map_controls/system_assets.cljs
+++ src/cljs/pyregence/components/map_controls/system_assets.cljs
@@ -14,10 +14,11 @@
 (defn panel-section
   []
   (let [orgs-with-system-assets (r/atom nil)
-        device-info [{:device "fuse"       :color "red"}
-                     {:device "substation" :color "blue"}
-                     {:device "recloser"   :color "green"}
-                     {:device "breaker"    :color "orange"}]]
+        device-info             [{:device "fuse" :color "red"}
+                                 {:device "substation" :color "blue"}
+                                 {:device "recloser" :color "green"}
+                                 {:device "breaker" :color "orange"}]
+        ->id                    (fn [org_unique_id device] (str org_unique_id "-" device))]
     (r/create-class
      {:component-did-mount
       #(go
@@ -27,39 +28,47 @@
                              (let [{:keys [body success]}
                                    (<! (u-async/call-clj-async! "get-orgs-with-system-assets"))]
                                (when success (edn/read-string body)))))]
-           (let [geoserver "psps"
-                 info (for [{:keys [org_unique_id] :as o} orgs-with-system-assets
-                            {:keys [device] :as d}  device-info]
-                        (merge o d {:id (str org_unique_id "-" device) :source-layer (str org_unique_id "-devices")}))
-                 new-layers (for [{:keys [id device color source-layer]} info]
-                              {:id           id
-                               :source       id
-                               :source-layer source-layer
-                               :filter       ["==" ["get" "DVC_TYPE"] device]
-                               :type         "circle"
-                               :paint        {:circle-radius  6
-                                              :circle-color   color
-                                              :circle-opacity 1}})
-                 new-sources (reduce
-                              (fn [new-sources {:keys [id org_unique_id source-layer]}]
-                                (assoc new-sources id (mb/mvt-source (str geoserver "-static_" org_unique_id "%3A" source-layer) geoserver)))
-                              {}
-                              info)]
-             (mb/update-style! (mb/get-style) :new-sources new-sources :new-layers new-layers)
-             (doseq [{:keys [id source-layer]} info]
-               (mb/set-visible-by-title! id false)
-               (mb/add-feature-highlight! id id
-                                          :click-fn
-                                          (fn [feature lnglat]
-                                            (mb/init-popup! "system-assets" lnglat
-                                                            [popup
-                                                             (let [{:keys [DVC_ID DVC_TYPE]}
-                                                                   (js->clj (gobj/get feature "properties")
-                                                                            :keywordize-keys true)]
-                                                               {:header  DVC_ID
-                                                                :options [{:label "Device Type" :value DVC_TYPE}]})]
-                                                            {:width "200px"}))
-                                          :source-layer source-layer)))))
+           (let [add-layers-callback
+                 (fn []
+                   (println "add layers!")
+                   (let [geoserver   "psps"
+                         device-meta (for [{:keys [org_unique_id] :as o} orgs-with-system-assets
+                                           {:keys [device] :as d}        device-info]
+                                       (merge o d {:id (->id org_unique_id device) :source-layer (str org_unique_id "-devices")}))
+                         new-layers  (for [{:keys [id device color source-layer]} device-meta]
+                                       {:id           id
+                                        :source       id
+                                        :source-layer source-layer
+                                        :filter       ["==" ["get" "DVC_TYPE"] device]
+                                        :type         "circle"
+                                        :paint        {:circle-radius  6
+                                                       :circle-color   color
+                                                       :circle-opacity 1}})
+                         new-sources (reduce
+                                      (fn [new-sources {:keys [id org_unique_id source-layer]}]
+                                        (assoc new-sources id (mb/mvt-source (str geoserver "-static_" org_unique_id "%3A" source-layer) geoserver)))
+                                      {}
+                                      device-meta)]
+                     (mb/update-style! (mb/get-style)  :new-sources new-sources :new-layers (vec new-layers))
+                     (doseq [{:keys [id source-layer]} device-meta]
+                       (mb/set-visible-by-title! id false)
+                       (mb/add-feature-highlight! id id
+                                                  :click-fn
+                                                  (fn [feature lnglat]
+                                                    (mb/init-popup! "system-assets" lnglat
+                                                                    [popup
+                                                                     (let [{:keys [DVC_ID DVC_TYPE]}
+                                                                           (js->clj (gobj/get feature "properties")
+                                                                                    :keywordize-keys true)]
+                                                                       {:header  DVC_ID
+                                                                        :options [{:label "Device Type" :value DVC_TYPE}]})]
+                                                                    {:width "200px"}))
+                                                  :source-layer source-layer))))]
+             (if (.isStyleLoaded @mb/the-map)
+               (add-layers-callback)
+               (.once @mb/the-map "style.load" (fn []
+                                                 (println "once!!")
+                                                 (add-layers-callback)))))))
 
       :reagent-render
       #(when-let [orgs-with-system-assets (seq @orgs-with-system-assets)]
@@ -69,14 +78,14 @@
            [:div {:style {:display "flex" :flex-direction "column" :margin-top ".25rem"}}
             [:label "System Assets"]
             (for [{:keys [org_unique_id org_name]} orgs-with-system-assets
-                  {:keys [device]} device-info
-                  :let                        [id (str org_unique_id "-" device)
-                                               opt-label (str (str/capitalize device) "s " "(" org_name ")")
-                                               ;;TODO what needs to be in this layer?
-                                               layer {:device       device
-                                                      :id           id
-                                                      :z-index      110
-                                                      :filter-set   #{org_unique_id}}]]
+                  {:keys [device]}                 device-info
+                  :let                             [id (->id org_unique_id device)
+                                                    opt-label (str (str/capitalize device) "s " "(" org_name ")")
+                                                    layer {:id         id
+                                                      ;;TODO does it need opt-label...i bet not
+                                                           :opt-label  opt-label
+                                                           :z-index    110
+                                                           :filter-set #{org_unique_id}}]]
               ^{:key id}
               [u/option
                {:id    id
