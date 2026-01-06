dbcdf0914753c6461135d96fd039631b4118fe78
drew verlee <drew.verlee@gmail.com>
2026-01-06 13:18:15 -0600

    Add Source and Layer on toggle to avoid race.

--- src/cljs/pyregence/components/map_controls/system_assets.cljs
+++ src/cljs/pyregence/components/map_controls/system_assets.cljs
@@ -17,48 +17,14 @@
         device-info             [{:device "fuse" :color "red"}
                                  {:device "substation" :color "blue"}
                                  {:device "recloser" :color "green"}
-                                 {:device "breaker" :color "orange"}]
-        ->id                    (fn [org_unique_id device] (str org_unique_id "-" device))]
+                                 {:device "breaker" :color "orange"}]]
     (r/create-class
      {:component-did-mount
       #(go
-         (when-let [orgs-with-system-assets
-                    (seq
-                     (reset! orgs-with-system-assets
-                             (let [{:keys [body success]}
-                                   (<! (u-async/call-clj-async! "get-orgs-with-system-assets"))]
-                               (when success (edn/read-string body)))))]
-           (doseq [{:keys [org_unique_id]} orgs-with-system-assets
-                   {:keys [device color]}  device-info
-                   :let                    [id (->id org_unique_id device)
-                                            source-layer (str org_unique_id "-devices")
-                                            layer-name (str "psps-static_" org_unique_id "%3A" source-layer)]]
-
-             (when-not (.getSource @mb/the-map id)
-               (.addSource @mb/the-map id (clj->js (mb/mvt-source layer-name "psps"))))
-
-             (when-not (.getLayer @mb/the-map id)
-               (.addLayer @mb/the-map (clj->js {:id           id
-                                                :source       id
-                                                :source-layer source-layer
-                                                :filter       ["==" ["get" "DVC_TYPE"] device]
-                                                :type         "circle"
-                                                :paint        {:circle-radius 6 :circle-color color}})))
-
-             (mb/set-visible-by-title! id false)
-             (mb/add-feature-highlight! id id
-                                        :click-fn
-                                        (fn [feature lnglat]
-                                          (mb/init-popup! "system-assets" lnglat
-                                                          [popup
-                                                           (let [{:keys [DVC_ID DVC_TYPE]}
-                                                                 (js->clj (gobj/get feature "properties")
-                                                                          :keywordize-keys true)]
-                                                             {:header  DVC_ID
-                                                              :options [{:label "Device Type" :value DVC_TYPE}]})]
-                                                          {:width "200px"}))
-                                        :source-layer source-layer))))
-
+         (reset! orgs-with-system-assets
+                 (let [{:keys [body success]}
+                       (<! (u-async/call-clj-async! "get-orgs-with-system-assets"))]
+                   (when success (edn/read-string body)))))
       :reagent-render
       #(when-let [orgs-with-system-assets (seq @orgs-with-system-assets)]
          [u/collapsible-panel-section
@@ -67,12 +33,12 @@
            [:div {:style {:display "flex" :flex-direction "column" :margin-top ".25rem"}}
             [:label "System Assets"]
             (for [{:keys [org_unique_id org_name]} orgs-with-system-assets
-                  {:keys [device]}                 device-info
-                  :let                             [id (->id org_unique_id device)
+                  {:keys [device color]}           device-info
+                  :let                             [id (str org_unique_id "-" device)
+                                                    source-layer (str org_unique_id "-devices")
                                                     opt-label (str (str/capitalize device) "s " "(" org_name ")")
+                                                    layer-name (str "psps-static_" org_unique_id "%3A" source-layer)
                                                     layer {:id         id
-                                                      ;;TODO does it need opt-label...i bet not
-                                                           :opt-label  opt-label
                                                            :z-index    110
                                                            :filter-set #{org_unique_id}}]]
               ^{:key id}
@@ -82,9 +48,29 @@
                 :on-change
                 (fn [show?]
                   (let [show? (swap! show? not)]
-                    (mb/set-visible-by-title! id show?)
+                    (when show?
+                      (when-not (.getSource @mb/the-map id)
+                        (.addSource @mb/the-map id (clj->js (mb/mvt-source layer-name "psps"))))
+
+                      (when-not (.getLayer @mb/the-map id)
+                        (.addLayer @mb/the-map (clj->js {:id     id :source id :source-layer source-layer
+                                                         :filter ["==" ["get" "DVC_TYPE"] device]
+                                                         :type   "circle"
+                                                         :paint  {:circle-radius 6 :circle-color color}}))
+                        (mb/add-feature-highlight! id id :click-fn (fn [feature lnglat]
+                                                                     (mb/init-popup! "system-assets" lnglat
+                                                                                     [popup
+                                                                                      (let [{:keys [DVC_ID DVC_TYPE]}
+                                                                                            (js->clj (gobj/get feature "properties")
+                                                                                                     :keywordize-keys true)]
+                                                                                        {:header  DVC_ID
+                                                                                         :options [{:label "Device Type" :value DVC_TYPE}]})]
+                                                                                     {:width "200px"})) :source-layer source-layer)))
+                    (.setLayoutProperty @mb/the-map id "visibility" (if show? "visible" "none"))
                     (if show?
                       (do
                         (swap! !/*optional-layers assoc id layer)
                         (reset! !/most-recent-optional-layer layer))
-                      (reset! !/most-recent-optional-layer {}))))}])]]])})))
+                      (do
+                        (swap! !/*optional-layers dissoc id) ;; Clean up the map
+                        (reset! !/most-recent-optional-layer {})))))}])]]])})))
