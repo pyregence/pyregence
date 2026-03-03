76fa96440240e3e93463ffefd248e1d496a7cde9
Daniel Silva <dsilva@sig-gis.com>
2026-03-03 14:16:01 -0300

    fixup

--- src/clj/pyregence/capabilities.clj
+++ src/clj/pyregence/capabilities.clj
@@ -231,7 +231,7 @@
                           (re-matches #"([a-z|-]+_[a-z0-9|-]+_)\d{8}_\d{2}:([A-Za-z0-9|-]+\d*_)+\d{8}_\d{6}" full-name)
                           (merge-fn (split-risk-weather-psps-layer-name full-name))
                           (and (re-matches #"[a-z|-]+_[a-z|-]+[a-z|\d|-]*_\d{8}_\d{6}:([a-z|-]+_){2}\d{2}_[a-z|-]+" full-name)
-                               (not (str/includes? full-name "match-drop")))
+                               (or (get-config :triangulum.views/client-keys :features :match-drop) (not (str/includes? full-name "match-drop"))))
                           (merge-fn (split-fire-spread-forecast-layer-name full-name))
                           (and (str/includes? full-name "isochrones")
                                (re-matches #"([a-z|-]+_)[a-z|-]+[a-z|\d|-]*_\d{8}_\d{6}:([a-z|-]+_){2}\d{2}_isochrones_[a-z|\d|-]*_\d{8}_\d{6}_\d{2}" full-name))
