1c2c866ac78e8183ce5d48db9f42e91084ab55bd
drew verlee <drew.verlee@gmail.com>
2026-07-30 13:20:59 -0500

    Remove duplicate validations

--- src/cljs/pyregence/pages/register.cljs
+++ src/cljs/pyregence/pages/register.cljs
@@ -57,12 +57,6 @@
                         (when-not (= @email @re-email)
                           "The emails you have entered do not match.")
 
-                        (when (< (count @password) 12)
-                          "Your password must be at least 12 characters long.")
-
-                        (when (< 64 (count @password))
-                          "Your password must be less than 64 characters long.")
-
                         (when-not (= @password @re-password)
                           "The passwords you have entered do not match.")
 
