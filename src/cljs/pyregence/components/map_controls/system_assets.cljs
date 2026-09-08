3b8bd5f7161a2736f55f6c505bc3c14aaf0a2677
drew verlee <drew.verlee@gmail.com>
2026-09-08 11:24:34 -0500

    wip add switch and sectionalizer
    wip because untested and needs colors.

--- src/cljs/pyregence/components/map_controls/system_assets.cljs
+++ src/cljs/pyregence/components/map_controls/system_assets.cljs
@@ -14,10 +14,12 @@
 (defn panel-section
   []
   (let [orgs-with-system-assets (r/atom nil)
-        device-info             [{:device "fuse"       :color "red"    :hover-color "#9C2007" :selected-color "#F87C63"}
-                                 {:device "substation" :color "blue"   :hover-color "#072F9C" :selected-color "#638BF8"}
-                                 {:device "recloser"   :color "green"  :hover-color "#177005" :selected-color "#7CF863"}
-                                 {:device "breaker"    :color "orange" :hover-color "#9C5907" :selected-color "#FACA8F"}]]
+        device-info             [{:device "fuse"              :color "red"    :hover-color "#9C2007" :selected-color "#F87C63"}
+                                 {:device "substation"        :color "blue"   :hover-color "#072F9C" :selected-color "#638BF8"}
+                                 {:device "recloser"          :color "green"  :hover-color "#177005" :selected-color "#7CF863"}
+                                 {:device "breaker"           :color "orange" :hover-color "#9C5907" :selected-color "#FACA8F"}
+                                 {:device "switch"            :color "TODO"   :hover-color "TODO"    :selected-color "TODO"}
+                                 {:device "sectionalizer"     :color "TODO"   :hover-color "TODO"    :selected-color "TODO"}]]
     (go
       (reset! orgs-with-system-assets
               (let [{:keys [body success]}
